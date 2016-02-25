437a7cedc28c8b06b31848dbe76a3d4cd9b3aa91
liuyaxin <yaxinliu@galaxyinternet>
2016-02-25 18:14:12 +0800

    tag:提交日程相关类

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/user/SopUserScheduleDao.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.dao.user;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.user.SopUserSchedule;
+
+public interface SopUserScheduleDao extends BaseDao<SopUserSchedule, Long>{
+
+}
---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/model/user/SopUserSchedule.java
@@ -0,0 +1,76 @@
+package com.galaxyinternet.model.user;
+
+import java.sql.Timestamp;
+
+import com.galaxyinternet.framework.core.model.BaseEntity;
+
+public class SopUserSchedule extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 用户id
+	 */
+	private int userId;
+
+	/**
+	 * 事项类型 : 任务 会议 访谈...
+	 */
+	private int itemType;
+
+	/**
+	 * 内容
+	 */
+	private String content;
+
+	/**
+	 * 字典 优先级
+	 */
+	private String itemOrder;
+
+	/**
+	 * 日期
+	 */
+	private Timestamp itemDate;
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setItemType(int itemType) {
+		this.itemType = itemType;
+	}
+
+	public int getItemType() {
+		return itemType;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setItemOrder(String itemOrder) {
+		this.itemOrder = itemOrder;
+	}
+
+	public String getItemOrder() {
+		return itemOrder;
+	}
+
+	public void setItemDate(Timestamp itemDate) {
+		this.itemDate = itemDate;
+	}
+
+	public Timestamp getItemDate() {
+		return itemDate;
+	}
+
+}
