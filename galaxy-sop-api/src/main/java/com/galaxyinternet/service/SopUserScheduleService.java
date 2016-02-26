a42ba71ae6fc277dbc4f00324953c937178d4529
liuyaxin <yaxinliu@galaxyinternet>
2016-02-26 17:02:33 +0800

    tag:添加我的日程增加、删、改、查接口

--- galaxy-sop-api/src/main/java/com/galaxyinternet/service/SopUserScheduleService.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/service/SopUserScheduleService.java
@@ -1,8 +1,13 @@
 package com.galaxyinternet.service;
 
+import java.util.List;
+
+import com.galaxyinternet.bo.SopUserScheduleBo;
 import com.galaxyinternet.framework.core.service.BaseService;
 import com.galaxyinternet.model.soptask.SopUserSchedule;
 
 public interface SopUserScheduleService extends BaseService<SopUserSchedule>{
 
+	public List<SopUserScheduleBo> selectSopUserScheduleByTime(Long currentTime);
+	
 }
--- galaxy-sop-server/src/main/java/com/galaxyinternet/soptask/controller/SopUserScheduleController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/soptask/controller/SopUserScheduleController.java
@@ -1,42 +1,86 @@
 package com.galaxyinternet.soptask.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.galaxyinternet.bo.SopUserScheduleBo;
 import com.galaxyinternet.common.controller.BaseControllerImpl;
 import com.galaxyinternet.framework.core.model.ResponseData;
+import com.galaxyinternet.framework.core.model.Result;
 import com.galaxyinternet.framework.core.service.BaseService;
 import com.galaxyinternet.model.soptask.SopUserSchedule;
 import com.galaxyinternet.service.SopUserScheduleService;
 
 @Controller
 @RequestMapping("/galaxy/userSchedule")
-public class SopUserScheduleController extends BaseControllerImpl<SopUserSchedule, SopUserScheduleBo> {
+public class SopUserScheduleController extends
+		BaseControllerImpl<SopUserSchedule, SopUserScheduleBo> {
+	final Logger logger = LoggerFactory
+			.getLogger(SopUserScheduleController.class);
 
 	@Autowired
 	private SopUserScheduleService sopUserScheduleService;
-	
+
 	@Override
 	protected BaseService<SopUserSchedule> getBaseService() {
 		// TODO Auto-generated method stub
 		return this.sopUserScheduleService;
 	}
-	
-	
+
+	/***
+	 * 添加|修改我的日程 status 1:添加,2:修改
+	 * 
+	 * @param sopUserSchedule
+	 * @return
+	 */
 	@ResponseBody
-	@RequestMapping(value = "/addUserSchedule", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseData<SopUserSchedule> addUserSchedule() {
-		
+	@RequestMapping(value = "/addOrUpdateSopUserSchedule/{status}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseData<SopUserSchedule> addUserSchedule(
+			@RequestBody SopUserSchedule sopUserSchedule,
+			@PathVariable String status) {
+
 		ResponseData<SopUserSchedule> responseBody = new ResponseData<SopUserSchedule>();
-		SopUserSchedule sop=new SopUserSchedule();
-		sop.setContent("test88");
-		sop.setUserId(6);
-		sopUserScheduleService.insert(sop);
-		
+		Result result = new Result();
+
+		if ("1".equals(status)) {
+			// 获取用户id
+			sopUserSchedule.setUserId(0);
+			sopUserScheduleService.insert(sopUserSchedule);
+			result.setMessage("添加日程成功!");
+		} else {
+			sopUserScheduleService.updateById(sopUserSchedule);
+			result.setMessage("修改日程成功!");
+		}
+
+		responseBody.setResult(result);
+		return responseBody;
+	}
+
+	/***
+	 * 获取我的日程前三条信息
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/selectSopUserSchedule", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	public ResponseData<SopUserSchedule> selectUserScheduleByTime() {
+
+		ResponseData<SopUserSchedule> responseBody = new ResponseData<SopUserSchedule>();
+		Result result = new Result();
+		Long currentTime = System.currentTimeMillis();
+		List<SopUserScheduleBo> list = sopUserScheduleService
+				.selectSopUserScheduleByTime(currentTime);
+		result.setMessage(list);
+		responseBody.setResult(result);
+
 		return responseBody;
 	}
 
