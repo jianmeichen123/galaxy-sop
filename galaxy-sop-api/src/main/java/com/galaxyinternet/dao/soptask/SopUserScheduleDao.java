6ffd9582d20c3bf53db1f25cdb9f4d59d48555c2
liuyaxin <yaxinliu@galaxyinternet>
2016-02-26 11:41:00 +0800

    tag:添加日程相关类

--- galaxy-sop-api/src/main/java/com/galaxyinternet/dao/soptask/SopUserScheduleDao.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/soptask/SopUserScheduleDao.java
@@ -1,7 +1,7 @@
-package com.galaxyinternet.dao.user;
+package com.galaxyinternet.dao.soptask;
 
 import com.galaxyinternet.framework.core.dao.BaseDao;
-import com.galaxyinternet.model.user.SopUserSchedule;
+import com.galaxyinternet.model.soptask.SopUserSchedule;
 
 public interface SopUserScheduleDao extends BaseDao<SopUserSchedule, Long>{
 
