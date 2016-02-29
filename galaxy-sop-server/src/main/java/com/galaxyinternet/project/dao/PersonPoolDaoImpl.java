b7b06e60236383179b429ddb900b2e1c43ff9fef
Administrator <Administrator@yangshuhua>
2016-02-29 18:26:03 +0800

    person-pool

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/dao/PersonPoolDaoImpl.java
@@ -0,0 +1,11 @@
+package com.galaxyinternet.project.dao;
+
+import org.springframework.stereotype.Repository;
+import com.galaxyinternet.dao.project.PersonPoolDao;
+import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.model.project.PersonPool;
+
+@Repository("personPoolDaoImpl")
+public class PersonPoolDaoImpl extends BaseDaoImpl<PersonPool, Long> implements PersonPoolDao{
+
+}
