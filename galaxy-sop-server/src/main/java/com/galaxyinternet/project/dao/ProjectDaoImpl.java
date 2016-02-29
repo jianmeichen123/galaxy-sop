b4ae6ec4eee0112219cd1072cb963bef0064c359
陈建梅 <jianmeichen@galaxyinternet.com>
2016-02-29 14:48:46 +0800

    根据条查询查询项目列表

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/dao/ProjectDaoImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/dao/ProjectDaoImpl.java
@@ -1,14 +1,32 @@
 package com.galaxyinternet.project.dao;
 
-import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import com.galaxyinternet.bo.project.ProjectBo;
 import com.galaxyinternet.dao.project.ProjectDao;
+import com.galaxyinternet.framework.core.constants.SqlId;
 import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.framework.core.exception.DaoException;
+import com.galaxyinternet.framework.core.utils.BeanUtils;
 import com.galaxyinternet.model.project.Project;
 
 
 @Repository("projectDao")
 public class ProjectDaoImpl extends BaseDaoImpl<Project, Long> implements ProjectDao {
+	@Override
+	public List<Project> selectProjectByMap(ProjectBo query) {
+		Assert.notNull(query);
+		Map<String, Object> params = BeanUtils.toMap(query);
+		try {
+		return sqlSessionTemplate.selectList(getSqlName(SqlId.SQL_SELECT),params);
+		} catch (Exception e) {
+			throw new DaoException(String.format("查询对象列表出错：%s", getSqlName(SqlId.SQL_SELECT)), e);
+		}
+	}
 
 
 }
