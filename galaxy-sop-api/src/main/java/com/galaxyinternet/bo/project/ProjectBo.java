39f17ad3359c06389326853dfbe9f6f0b58f09bb
陈建梅 <jianmeichen@galaxyinternet.com>
2016-02-29 14:31:13 +0800

    项目扩展类里面，添加项目扩展字段---nameLike，根据项目名称或者项目创建人模糊查询项目

--- galaxy-sop-api/src/main/java/com/galaxyinternet/bo/project/ProjectBo.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/project/ProjectBo.java
@@ -6,6 +6,8 @@ public class ProjectBo extends Project {
 	private static final long serialVersionUID = 1L;
 
 	private String extendFiled;// 业务对象中扩展的字段
+	
+	private String nameLike;//模糊查询条件匹配
 
 	public String getExtendFiled() {
 		return extendFiled;
@@ -15,4 +17,14 @@ public class ProjectBo extends Project {
 		this.extendFiled = extendFiled;
 	}
 
+	public String getNameLike() {
+		return nameLike;
+	}
+
+	public void setNameLike(String nameLike) {
+		this.nameLike = nameLike;
+	}
+	
+	
+  
 }
