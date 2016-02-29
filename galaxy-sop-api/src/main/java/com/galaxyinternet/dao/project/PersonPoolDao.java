7777898cdd22f5b4e19369127103dff80c69130b
Administrator <Administrator@yangshuhua>
2016-02-29 17:10:00 +0800

    person pool

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/project/PersonPoolDao.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.dao.project;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.project.Project;
+
+public interface PersonPoolDao extends BaseDao<Project, Long> {
+
+}
