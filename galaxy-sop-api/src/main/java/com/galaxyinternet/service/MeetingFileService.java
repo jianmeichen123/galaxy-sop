df953c71b4e06a50c09904db969a6df1c60d71bb
zhangfeng <fengzhang@falaxyinternet.com>
2016-02-29 16:14:49 +0800

    interviewFile相关类删除

--- galaxy-sop-api/src/main/java/com/galaxyinternet/service/MeetingFileService.java
+++
@@ -1,8 +0,0 @@
-package com.galaxyinternet.service;
-
-import com.galaxyinternet.framework.core.service.BaseService;
-import com.galaxyinternet.model.project.MeetingFile;
-
-public interface MeetingFileService extends BaseService<MeetingFile> {
-
-}
