d67e46a6ae3d3070b65e111a0579820bb07bd09e
Administrator <Administrator@yangshuhua>
2016-03-01 13:41:09 +0800

    remove user model

--- galaxy-sop-api/src/main/java/com/galaxyinternet/dao/user/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.galaxyinternet.dao.user;
-
-import com.galaxyinternet.framework.core.dao.BaseDao;
-import com.galaxyinternet.model.user.User;
-
-public interface UserDao extends BaseDao<User, Long> {
-}
