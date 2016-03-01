5d47f1ec6c3101397c1e21d5f93ab86aa7f294a5
Administrator <Administrator@yangshuhua>
2016-03-01 12:52:54 +0800

    dubbo

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.galaxyinternet.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/galaxy")
+public class IndexController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+}
