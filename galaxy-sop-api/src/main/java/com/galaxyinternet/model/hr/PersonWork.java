5164d9635a77bf3298f42adee1b9c30d0e69eb6b
gxc <xiangchengao@galaxyinternet.com>
2016-03-03 18:11:59 +0800

    更新文件

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/model/hr/PersonWork.java
@@ -0,0 +1,124 @@
+package com.galaxyinternet.model.hr;
+
+import java.util.Date;
+
+import com.galaxyinternet.framework.core.model.BaseEntity;
+
+public class PersonWork extends BaseEntity{
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;//人力_工作经历项的ID 
+	private Long personId;//关联人力资源ID
+	private String companyName;//公司名称
+	private String workDepart;//所在部门
+	private String workPosition;//所处职位
+	private String workContent;//工作内容
+	private String workEffect;//工作业绩
+	private Double workEmolument;//薪酬
+	private Date beginWork;//入职时间 
+	private Date overWork;//离职时间
+	private String leaveReason;//离职原因
+	private String leaderPosition;//上级姓名
+	private String leaderPhone;//上级电话
+	private String colleagueName;//同事姓名
+	private String colleaguePosition;//同事职位
+	private String colleaguePhone;//同事电话
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getPersonId() {
+		return personId;
+	}
+	public void setPersonId(Long personId) {
+		this.personId = personId;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getWorkDepart() {
+		return workDepart;
+	}
+	public void setWorkDepart(String workDepart) {
+		this.workDepart = workDepart;
+	}
+	public String getWorkPosition() {
+		return workPosition;
+	}
+	public void setWorkPosition(String workPosition) {
+		this.workPosition = workPosition;
+	}
+	public String getWorkContent() {
+		return workContent;
+	}
+	public void setWorkContent(String workContent) {
+		this.workContent = workContent;
+	}
+	public String getWorkEffect() {
+		return workEffect;
+	}
+	public void setWorkEffect(String workEffect) {
+		this.workEffect = workEffect;
+	}
+	
+	public Double getWorkEmolument() {
+		return workEmolument;
+	}
+	public void setWorkEmolument(Double workEmolument) {
+		this.workEmolument = workEmolument;
+	}
+	public Date getBeginWork() {
+		return beginWork;
+	}
+	public void setBeginWork(Date beginWork) {
+		this.beginWork = beginWork;
+	}
+	public Date getOverWork() {
+		return overWork;
+	}
+	public void setOverWork(Date overWork) {
+		this.overWork = overWork;
+	}
+	public String getLeaveReason() {
+		return leaveReason;
+	}
+	public void setLeaveReason(String leaveReason) {
+		this.leaveReason = leaveReason;
+	}
+	public String getLeaderPosition() {
+		return leaderPosition;
+	}
+	public void setLeaderPosition(String leaderPosition) {
+		this.leaderPosition = leaderPosition;
+	}
+	public String getLeaderPhone() {
+		return leaderPhone;
+	}
+	public void setLeaderPhone(String leaderPhone) {
+		this.leaderPhone = leaderPhone;
+	}
+	public String getColleagueName() {
+		return colleagueName;
+	}
+	public void setColleagueName(String colleagueName) {
+		this.colleagueName = colleagueName;
+	}
+	public String getColleaguePosition() {
+		return colleaguePosition;
+	}
+	public void setColleaguePosition(String colleaguePosition) {
+		this.colleaguePosition = colleaguePosition;
+	}
+	public String getColleaguePhone() {
+		return colleaguePhone;
+	}
+	public void setColleaguePhone(String colleaguePhone) {
+		this.colleaguePhone = colleaguePhone;
+	}
+	
+}
