31d448ab57f64cf4e1b4ac21d894fee1c8a22651
gxc <xiangchengao@galaxyinternet.com>
2016-03-03 18:11:41 +0800

    文件删除

--- galaxy-sop-api/src/main/java/com/galaxyinternet/dao/HR/PersonInvestDao.java
+++
@@ -1,8 +0,0 @@
-package com.galaxyinternet.dao.HR;
-
-import com.galaxyinternet.framework.core.dao.BaseDao;
-import com.galaxyinternet.model.HR.personInvest;
-
-public interface PersonInvestDao extends BaseDao<personInvest, Long> {
-
-}
