fc324502ae33bc1db60bc6c25c4e19d50040f9cd
gxc <xiangchengao@galaxyinternet.com>
2016-03-03 09:40:40 +0800

    完善个人简历

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/project/personInvestBo.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.bo.project;
+
+import com.galaxyinternet.model.HR.personInvest;
+
+public class personInvestBo extends personInvest{
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/project/personWorkBo.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.bo.project;
+
+import com.galaxyinternet.model.HR.personWork;
+
+public class personWorkBo extends personWork{
+
+	private static final long serialVersionUID = 1L;
+}
