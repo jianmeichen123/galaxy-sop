710d9a3bc09cb597fcf54caf69dfd1bd34349a2f
Administrator <Administrator@yangshuhua>
2016-03-03 17:52:38 +0800

    ysh

--- galaxy-sop-api/src/main/java/com/galaxyinternet/service/ProjectService.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/service/ProjectService.java
@@ -4,5 +4,11 @@ import com.galaxyinternet.framework.core.service.BaseService;
 import com.galaxyinternet.model.project.Project;
 
 public interface ProjectService extends BaseService<Project> {
+	
+	/**
+	 * 添加项目
+	 * 对于每个新添加的项目来讲，整个生命周期中所关联的文档已确定，这里需要一并添加
+	 */
+	public long newProject(Project project) throws Exception;
 
 }
