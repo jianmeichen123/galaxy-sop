4636b35a5cf3cec48e752ce88646e2ac42423544
gxc <xiangchengao@galaxyinternet.com>
2016-03-03 09:42:15 +0800

    完善简历dao

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/HR/PersonWorkDao.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.dao.HR;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.HR.personWork;
+
+public interface PersonWorkDao extends BaseDao<personWork, Long> {
+
+}
