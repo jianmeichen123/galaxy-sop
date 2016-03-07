811ff1a89ca20e7d2a00d5358052d434018c7720
chenjianmei <jianmeichen@galaxyinternet.com>
2016-03-07 18:12:58 +0800

    修改路径

---
+++ galaxy-sop-server/src/main/webapp/js/platformUrl.js
@@ -0,0 +1,65 @@
+var Constants = {
+	platformEndpointURL : endpointObj["galaxy.project.platform.endpoint"],
+	reportEndpointURL : endpointObj["galaxy.project.report.endpoint"]
+}
+
+var platformUrl = {
+	/**
+	 * 验证登录
+	 */
+	createMenus : "./common/menu/",
+	/**
+	 * 获取项目编码接口地址
+	 */
+	getProjectCode : "./project/cpc",
+	/**
+	 * 添加项目
+	 */
+	addProject : "./project/ap",
+	/**
+	 * 获取项目详情
+	 */
+	detailProject : "./project/sp/",
+	/**
+	 * 修改项目信息
+	 */
+	updateProject : "./project/up",
+	/**
+	 * 添加团队成员
+	 */
+	addPerson : "./project/app",
+	/**
+	 * 跳转到首页
+	 */
+	toIndex : Constants.platformEndpointURL + "/galaxy/redirect",
+	/**
+	 * 查询模板列表
+	 */
+	queryTemplate : "/galaxy/template/queryTemplate",
+	/**
+	 * 查询模板相关数据
+	 */
+	getTempRelatedData:"/galaxy/template/getRelatedData",
+	/**
+	 * 模板上传
+	 */
+	tempUpload:"/galaxy/template/upload",
+	/**
+	 * 模板保存
+	 */
+	tempSave:"/galaxy/template/save",
+	/**
+	 * 模板下载
+	 */
+	tempDownload:"/galaxy/template/download",
+	
+	/**
+	 * 修改团队成员
+	 */
+	updatePerson : "/galaxy/project/upp",
+	
+}
+
+/**
+ * how to use? location.href = platformUrl.login
+ */
