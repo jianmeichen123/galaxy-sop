12139cb00638553ddcda9c09f2b78bd1dceb5377
liuyaxin <yaxinliu@galaxyinternet>
2016-03-09 12:45:27 +0800

    update

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectProgressController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectProgressController.java
@@ -582,7 +582,7 @@ public class ProjectProgressController extends BaseControllerImpl<Project, Proje
 		List<String> fileworktypeList = new ArrayList<String>();
 		
 		Project project = projectService.queryById(pid);
-		proProgress = project.getProgress();
+		proProgress = project.getProjectProgress();
 		
 		//根据角色判断-显示文件上传列表
 		User user =(User)request.getSession().getAttribute(Constants.SESSION_USER_KEY);
