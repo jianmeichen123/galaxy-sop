56454822426d92cd3c3ff64744afe1bd3be8ada7
ysh <ysh@ysh-PC>
2016-03-09 10:25:22 +0800

    url

--- galaxy-sop-server/src/main/webapp/js/platformUrl.js
+++ galaxy-sop-server/src/main/webapp/js/platformUrl.js
@@ -2,32 +2,33 @@ var Constants = {
 	platformEndpointURL : endpointObj["galaxy.project.platform.endpoint"],
 	reportEndpointURL : endpointObj["galaxy.project.report.endpoint"]
 }
+var sopContentUrl = "http://127.0.0.1:8888/galaxy-sop-server";
 
 var platformUrl = {
 	/**
 	 * 验证登录
 	 */
-	createMenus : "./common/menu/",
+	createMenus : sopContentUrl + "/galaxy/common/menu/",
 	/**
 	 * 获取项目编码接口地址
 	 */
-	getProjectCode : "./project/cpc",
+	getProjectCode : sopContentUrl + "/galaxy/project/cpc",
 	/**
 	 * 添加项目
 	 */
-	addProject : "./project/ap",
+	addProject : sopContentUrl + "/galaxy/project/ap",
 	/**
 	 * 获取项目详情
 	 */
-	detailProject : "./project/sp/",
+	detailProject : sopContentUrl + "/galaxy/project/sp/",
 	/**
 	 * 修改项目信息
 	 */
-	updateProject : "./project/up",
+	updateProject : sopContentUrl + "/galaxy/project/up",
 	/**
 	 * 添加团队成员
 	 */
-	addPerson : "./project/app",
+	addPerson : sopContentUrl + "/galaxy/project/app",
 	/**
 	 * 跳转到首页
 	 */
@@ -35,74 +36,74 @@ var platformUrl = {
 	/**
 	 * 查询模板列表
 	 */
-	queryTemplate : "/galaxy/template/queryTemplate",
+	queryTemplate : sopContentUrl + "/galaxy/template/queryTemplate",
 	/**
 	 * 查询模板相关数据
 	 */
-	getTempRelatedData:"/galaxy/template/getRelatedData",
+	getTempRelatedData : sopContentUrl + "/galaxy/template/getRelatedData",
 	/**
 	 * 模板上传
 	 */
-	tempUpload:"/galaxy/template/upload",
+	tempUpload : sopContentUrl + "/galaxy/template/upload",
 	/**
 	 * 模板保存
 	 */
-	tempSave:"/galaxy/template/save",
+	tempSave : sopContentUrl + "/galaxy/template/save",
 	/**
 	 * 模板下载
 	 */
-	tempDownload:"/galaxy/template/download",
+	tempDownload : sopContentUrl + "/galaxy/template/download",
 	
 	/**
 	 * 修改团队成员
 	 */
-	updatePerson : "/galaxy/project/upp",
+	updatePerson : sopContentUrl + "/galaxy/project/upp",
 	
 	/**
 	 * 立项排期所有
 	 */
-    moreProjectMeeting : "/galaxy/home/moreProjectMeeting",
+    moreProjectMeeting : sopContentUrl + "/galaxy/home/moreProjectMeeting",
 	/**
 	 * top5立项会排期
 	 */
-	top5ProjectMeeting : "/galaxy/home/top5ProjectMeeting",
-	ProjectVoteWill: "/galaxy/home/ProjectVoteWill",
-	moreProjectVoteWill:"/galaxy/home/moreProjectVoteWill",
+	top5ProjectMeeting : sopContentUrl + "/galaxy/home/top5ProjectMeeting",
+	ProjectVoteWill: sopContentUrl + "/galaxy/home/ProjectVoteWill",
+	moreProjectVoteWill: sopContentUrl + "/galaxy/home/moreProjectVoteWill",
 	
 	/**
 	 * 添加股权结构
 	 */
-	addStock : "/galaxy/projectShares/addProjectShares",
+	addStock : sopContentUrl + "/galaxy/projectShares/addProjectShares",
 	
 	/**
 	 * 修改股权结构
 	 */
-	updateStock : "/galaxy/projectShares/updateProjectShares",
+	updateStock : sopContentUrl + "/galaxy/projectShares/updateProjectShares",
 	
 	
 	/**
 	 * 访谈查询 page
 	 */
-	selectViewPage : "/galaxy/project/progress/queryInterview",
+	selectViewPage : sopContentUrl + "/galaxy/project/progress/queryInterview",
 	
 	/**
 	 * 获取登录人的项目,阶段为：
 	 */
-	getUserPro : "/galaxy/project/progress/queryPerPro",
+	getUserPro : sopContentUrl + "/galaxy/project/progress/queryPerPro",
 	
 	/**
 	 * 访谈添加：附件上传
 	 */
-	saveInteverView : "/galaxy/project/progress/addInterview",
+	saveInteverView : sopContentUrl + "/galaxy/project/progress/addInterview",
 	
 	/**
 	 * 会议添加：
 	 */
-	saveMeet : "/galaxy/project/progress/addmeet",
+	saveMeet : sopContentUrl + "/galaxy/project/progress/addmeet",
 	/**
 	 * 根据findByParentCode查询数据字典
 	 */
-	dictFindByParentCode : "/galaxy/sopFile/getDictByParent",
+	dictFindByParentCode : sopContentUrl + "/galaxy/sopFile/getDictByParent",
 
 }
 
