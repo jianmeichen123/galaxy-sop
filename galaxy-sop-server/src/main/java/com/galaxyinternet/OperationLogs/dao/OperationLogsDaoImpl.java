e3a2ee23a11af1eec06029e76442099cc4abb39a
zhangfeng <fengzhang@falaxyinternet.com>
2016-03-16 10:55:22 +0800

    log类相关

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/OperationLogs/dao/OperationLogsDaoImpl.java
@@ -0,0 +1,18 @@
+﻿package com.galaxyinternet.OperationLogs.dao;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.galaxyinternet.dao.operationLog.OperationLogsDao;
+import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.model.operationLog.OperationLogs;
+
+@Repository("OperationLogsDao")
+public class OperationLogsDaoImpl extends BaseDaoImpl<OperationLogs, Long> implements OperationLogsDao  {
+
+	
+
+
+	
+	
+}
