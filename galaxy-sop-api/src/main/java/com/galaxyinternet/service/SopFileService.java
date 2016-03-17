0d22eca53bc369f7a8c94b9016fd0b987b476117
zhangfeng <fengzhang@falaxyinternet.com>
2016-03-17 10:43:15 +0800

    单纯更新file

--- galaxy-sop-api/src/main/java/com/galaxyinternet/service/SopFileService.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/service/SopFileService.java
@@ -2,7 +2,11 @@ package com.galaxyinternet.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.galaxyinternet.bo.sopfile.SopFileBo;
+import com.galaxyinternet.framework.core.model.Result;
+import com.galaxyinternet.framework.core.model.Result.Status;
 import com.galaxyinternet.framework.core.service.BaseService;
 import com.galaxyinternet.model.project.Project;
 import com.galaxyinternet.model.sopfile.SopFile;
@@ -36,7 +40,12 @@ public interface SopFileService extends BaseService<SopFile> {
 	public SopVoucherFile updateProve(SopVoucherFile sopVoucherFile,SopTask sopTask,Project project,Long userId,Long departmentId);
 	
 
-	
-	
+	/**
+	 * 单纯更新file
+	 * @param request
+	 * @param fid  file_id
+	 * @return Result.Status.ERROR\OK 失败\成功
+	 */
+	public Result updateFile(HttpServletRequest request, Long fid) throws Exception;
 	
 }
