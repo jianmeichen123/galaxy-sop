647700c696ed1ffc92d6d735d421309899e5342c
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-22 21:42:36 +0800

    还原  common.js

--- galaxy-sop-server/src/main/webapp/js/common.js
+++ galaxy-sop-server/src/main/webapp/js/common.js
@@ -32,16 +32,16 @@ function sendPostRequestByJsonObj(reqUrl, jsonObj, callbackFun) {
 			//alert("connetion error");
 		},
 		success : function(data) {
-			if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
+			/*if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
 					&&!data.hasOwnProperty("entity")&&!data.hasOwnProperty("pageVoList")
 					&&!data.hasOwnProperty("entityList")&&!data.hasOwnProperty("id")
 					&&!data.hasOwnProperty("header")
 			){
 				location.href = platformUrl.toLoginPage;
-			}
-			/*if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
-				location.href = platformUrl.toLoginPage;
 			}*/
+			if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
+				location.href = platformUrl.toLoginPage;
+			}
 			if (callbackFun) {
 				callbackFun(data);
 			}
@@ -98,16 +98,16 @@ function sendGetRequest(reqUrl, jsonObj, callbackFun) {
 			//alert("connetion error");
 		},
 		success : function(data) {
-			if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
+			/*if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
 					&&!data.hasOwnProperty("entity")&&!data.hasOwnProperty("pageVoList")
 					&&!data.hasOwnProperty("entityList")&&!data.hasOwnProperty("id")
 					&&!data.hasOwnProperty("header")
 			){
 				location.href = platformUrl.toLoginPage;
-			}
-			/*if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
-				location.href = platformUrl.toLoginPage;
 			}*/
+			if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
+				location.href = platformUrl.toLoginPage;
+			}
 			if (callbackFun) {
 				callbackFun(data);
 			}
@@ -144,16 +144,16 @@ function sendPostRequest(reqUrl, callbackFun) {
 			//alert("connetion error");
 		},
 		success : function(data) {
-			if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
+			/*if(!data.hasOwnProperty("result")&&!data.hasOwnProperty("pageList")
 					&&!data.hasOwnProperty("entity")&&!data.hasOwnProperty("pageVoList")
 					&&!data.hasOwnProperty("entityList")&&!data.hasOwnProperty("id")
 					&&!data.hasOwnProperty("header")
 			){
 				location.href = platformUrl.toLoginPage;
-			}
-			/*if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
-				location.href = platformUrl.toLoginPage;
 			}*/
+			if(data.hasOwnProperty("result")&&data.result.errorCode=="3"){
+				location.href = platformUrl.toLoginPage;
+			}
 			if (callbackFun) {
 				callbackFun(data);
 			}
