fb5e265a2d9c10954e3b5e145c3ddb8e843d9b18
swang <songwang@galaxyinternet.com>
2016-03-22 20:51:24 +0800

    wangsong fix bug #77【投资经理-模板管理】投资经理进入模板管理页面，操作栏的权限有错误

--- galaxy-sop-api/src/main/java/com/galaxyinternet/bo/template/SopTemplateBo.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/template/SopTemplateBo.java
@@ -1,10 +1,21 @@
 package com.galaxyinternet.bo.template;
 
+import java.util.List;
+
 import com.galaxyinternet.model.template.SopTemplate;
 
 public class SopTemplateBo extends SopTemplate {
 	private static final long serialVersionUID = 1L;
 	private Long[] ids;
+	private List<String> fileWorktypes;
+
+	public List<String>  getFileWorktypes() {
+		return fileWorktypes;
+	}
+
+	public void setFileWorktypes(List<String>  fileWorktypes) {
+		this.fileWorktypes = fileWorktypes;
+	}
 
 	public Long[] getIds() {
 		return ids;
