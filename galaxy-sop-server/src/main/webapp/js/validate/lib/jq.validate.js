f2d27c8eb0aa0e7af700630a45d9659e48786e99
ysh <ysh@ysh-PC>
2016-03-26 17:26:47 +0800

    优化

--- galaxy-sop-server/src/main/webapp/js/validate/lib/jq.validate.js
+++ galaxy-sop-server/src/main/webapp/js/validate/lib/jq.validate.js
@@ -38,7 +38,8 @@ $(function(){
 			MAIL : "^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$",
 			IDENTITY : "((11|12|13|14|15|21|22|23|31|32|33|34|35|36|37|41|42|43|44|45|46|50|51|52|53|54|61|62|63|64|65|71|81|82|91)\\d{4})((((19|20)(([02468][048])|([13579][26]))0229))|((20[0-9][0-9])|(19[0-9][0-9]))((((0[1-9])|(1[0-2]))((0[1-9])|(1\\d)|(2[0-8])))|((((0[1,3-9])|(1[0-2]))(29|30))|(((0[13578])|(1[02]))31))))((\\d{3}(x|X))|(\\d{4}))",
 			CHINESE : "^([\u4E00-\uFA29]|[\uE7C7-\uE7F3])*$",
-			URL : "^http[s]?://[\\w\\.\\-]+$"
+			URL : "^http[s]?://[\\w\\.\\-]+$",
+			LIMIT_NUMBER : "^(([1-9]+)|([0-9]+\.[0-9]{1,2}))$"
 		};
 		var flag=false;
 		if(para.rule=='OTHER') {//自定义的验证规则匹配
@@ -111,8 +112,7 @@ function beforeSubmit() {
 	 $.each($("[valType]"),function(i, n) {
 		 //清除可能已有的提示信息
 		 $(n).poshytip('hide');
-				 
-		if($(n).attr("valType")=='required') {//对不能为空的文本框进行验证
+		 if($(n).attr("valType")=='required') {//对不能为空的文本框进行验证
 			if($(n).val()=='') {
 			//显示tips			
 			$(n).poshytip('show');
@@ -133,10 +133,20 @@ function beforeSubmit() {
 				flag=false;
 			}
 		}
-		else {//对使用已定义规则的文本框进行验证			
-			if(!($(this).val()!=''&&$.Validator.match({data:$(this).val(), rule:$(this).attr('valType')}))) {
-				$(n).poshytip('show');
-				flag=false;
+		else {//对使用已定义规则的文本框进行验证	
+			if($(n).attr("isNULL") =='yes'){//可以为空
+				if($(this).val()!=''){
+					if(!$.Validator.match({data:$(this).val(), rule:$(this).attr('valType')})) {
+						$(n).poshytip('show');
+						flag=false;
+					}
+				}
+				
+			}else{
+				if(!($(this).val()!=''&&$.Validator.match({data:$(this).val(), rule:$(this).attr('valType')}))) {
+					$(n).poshytip('show');
+					flag=false;
+				}
 			}
 		}
 	 });
