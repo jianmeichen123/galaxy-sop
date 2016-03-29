c1499d8c47dbf7e886cd8668333a84ef86658aff
gxc <xiangchengao@galaxyinternet.com>
2016-03-29 15:49:02 +0800

    高管项目列表

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectController.java
@@ -312,26 +312,20 @@ public class ProjectController extends BaseControllerImpl<Project, ProjectBo> {
 	public ResponseData<Project> queryAllProjects(HttpServletRequest request, @RequestBody ProjectBo project) {
 		ResponseData<Project> responseBody = new ResponseData<Project>();
 		User user = (User) getUserFromSession(request);
-		List<Long> roleIdList = userRoleService.selectRoleIdByUserId(user.getId());		
+		List<Long> roleIdList = userRoleService.selectRoleIdByUserId(user.getId());
+		if(project.getProjectProgress()!=null&&project.getProjectProgress().equals("guanbi")){
+			project.setProjectStatus("meetingResult:3");
+			project.setProjectProgress(null);
+		}
 		try {
-			if(project.getProjectProgress()!=null&&project.getProjectProgress().equals("guanbi")){
-				project.setProjectStatus("meetingResult:3");
-				project.setProjectProgress(null);
-			}
 			if(roleIdList.contains(UserConstant.DSZ) || roleIdList.contains(UserConstant.CEO)){
 				/*Page<Project> pageProject = projectService.queryPageList(project,new PageRequest(project.getPageNum(), project.getPageSize()));
 				responseBody.setPageList(pageProject);
 				responseBody.setResult(new Result(Status.OK, ""));*/
-
 			}
-			/*if (roleIdList.contains(UserConstant.HHR)){
-				if(project.getProjectProgress()!=null&&project.getProjectProgress().equals("guanbi")){
-					project.setProjectStatus("meetingResult:3");
-					project.setProjectProgress(null);
-				}
+			if (roleIdList.contains(UserConstant.HHR)){
 				project.setProjectDepartid(user.getDepartmentId());
-			}*/
-
+			}
 			
 			Page<Project> pageProject = projectService.queryPageList(project,new PageRequest(project.getPageNum(), project.getPageSize()));
 			String hhrname="";
