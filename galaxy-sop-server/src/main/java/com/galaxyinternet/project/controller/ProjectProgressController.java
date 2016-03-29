36d81c0506d177aac510163324cbbdefc09705b3
ysh <ysh@ysh-PC>
2016-03-29 20:48:14 +0800

    dddd

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectProgressController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/controller/ProjectProgressController.java
@@ -778,7 +778,6 @@ public class ProjectProgressController extends BaseControllerImpl<Project, Proje
 	public ResponseData<SopFile> proFileInfo(HttpServletRequest request,@PathVariable("pid") Long pid,@PathVariable("progress") String progress) {
 		
 		ResponseData<SopFile> responseBody = new ResponseData<SopFile>();
-		dasdasdawsefsefgwer
 		List<String> fileworktypeList = new ArrayList<String>();
 		String proProgress="projectProgress:"+progress;
 		
