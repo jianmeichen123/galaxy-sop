a3420616877c5f6b544b5a4453860014123d83a4
ysh <ysh@ysh-PC>
2016-04-01 11:39:55 +0800

    优化

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/service/handler/TzyxsHandler.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/service/handler/TzyxsHandler.java
@@ -52,7 +52,7 @@ public class TzyxsHandler implements Handler {
 	public SopResult handler(ViewQuery query, Project project) throws Exception {
 		ProjectQuery q = (ProjectQuery) query;
 		if(q.getVoucherType() != null && q.getVoucherType().intValue() == 1){
-			//签署证明
+			//保存签署证明记录信息
 			SopVoucherFile qvf = new SopVoucherFile();
 			qvf.setProjectId(q.getPid());
 			qvf.setProjectProgress(q.getStage());
@@ -70,7 +70,7 @@ public class TzyxsHandler implements Handler {
 			fv.setFileName(q.getFileName());
 			fv.setFileSuffix(q.getSuffix());
 			sopVoucherFileDao.updateById(fv);
-			
+			//将file表记录状态修改为"已签署"
 			SopFile file = new SopFile();
 			file.setProjectId(q.getPid());
 			file.setProjectProgress(q.getStage());
