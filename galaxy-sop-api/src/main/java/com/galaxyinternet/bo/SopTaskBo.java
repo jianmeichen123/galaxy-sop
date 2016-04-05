85019a06d348185829aee13d68e6a952fa1d5738
gxc <xiangchengao@galaxyinternet.com>
2016-04-05 11:20:29 +0800

    删除无用的类

--- galaxy-sop-api/src/main/java/com/galaxyinternet/bo/SopTaskBo.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/SopTaskBo.java
@@ -91,30 +91,6 @@ public class SopTaskBo extends  SopTask{
 	public void setCaozuohtml(String caozuohtml) {
 		this.caozuohtml = caozuohtml;
 	}
-	
-	protected Integer pageSize;
-	protected Integer pageNum;
-
-	@Override
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
-
-	@Override
-	public Integer getPageSize() {
-		return this.pageSize;
-	}
-
-	@Override
-	public void setPageNum(Integer pageNum) {
-		this.pageNum = pageNum;
-
-	}
-
-	@Override
-	public Integer getPageNum() {
-		return this.pageNum;
-	}
 
 	public String getOrderRemark() {
 		return orderRemark;
