740ba9c6308c93f5a6bc3333a964a40f390cc63c
vincent <leizhang@galaxyinternet.com>
2016-04-11 18:05:44 +0800

    无更新

--- galaxy-sop-server/src/main/webapp/js/platformUrl.js
+++ galaxy-sop-server/src/main/webapp/js/platformUrl.js
@@ -1,11 +1,8 @@
 var Constants = {
 	platformEndpointURL : endpointObj["galaxy.project.platform.endpoint"],
-	reportEndpointURL : endpointObj["galaxy.project.report.endpoint"]
+	reportEndpointURL : endpointObj["galaxy.project.report.endpoint"],
+	sopEndpointURL : endpointObj["galaxy.project.sop.endpoint"],
 }
-var sopContentUrl = "http://fx.dev.galaxyinternet.com/sop";
-//var sopContentUrl = "http://fx.qa.galaxyinternet.com/sop";
-//var sopContentUrl = "http://127.0.0.1:8888/sop/";
-//var sopContentUrl = "http://fx.galaxyinternet.com/sop";
 
 var platformUrl = {
 		
@@ -24,35 +21,35 @@ var platformUrl = {
 	/**
 	 * 验证登录
 	 */
-	createMenus : sopContentUrl + "/galaxy/common/menu/",
+	createMenus : Constants.sopEndpointURL + "/galaxy/common/menu/",
 	/**
 	 * 获取项目编码接口地址
 	 */
-	getProjectCode : sopContentUrl + "/galaxy/project/cpc",
+	getProjectCode : Constants.sopEndpointURL + "/galaxy/project/cpc",
 	/**
 	 * 添加项目
 	 */
-	addProject : sopContentUrl + "/galaxy/project/ap",
+	addProject : Constants.sopEndpointURL + "/galaxy/project/ap",
 	/**
 	 * 关闭项目
 	 */
-	closeProject : sopContentUrl + "/galaxy/project/breakpro/",
+	closeProject : Constants.sopEndpointURL + "/galaxy/project/breakpro/",
 	/**
 	 * 获取事业部——高管查询select
 	 */
-	queryCheckLine : sopContentUrl + "/galaxy/project/queryCheckLine/",
+	queryCheckLine : Constants.sopEndpointURL + "/galaxy/project/queryCheckLine/",
 	/**
 	 * 获取项目详情
 	 */
-	detailProject : sopContentUrl + "/galaxy/project/sp/",
+	detailProject : Constants.sopEndpointURL + "/galaxy/project/sp/",
 	/**
 	 * 修改项目信息
 	 */
-	updateProject : sopContentUrl + "/galaxy/project/up",
+	updateProject : Constants.sopEndpointURL + "/galaxy/project/up",
 	/**
 	 * 添加团队成员
 	 */
-	addPerson : sopContentUrl + "/galaxy/project/app",
+	addPerson : Constants.sopEndpointURL + "/galaxy/project/app",
 	/**
 	 * 跳转到首页
 	 */
@@ -60,345 +57,350 @@ var platformUrl = {
 	/**
 	 * 查询模板列表
 	 */
-	queryTemplate : sopContentUrl + "/galaxy/template/queryTemplate",
+	queryTemplate : Constants.sopEndpointURL + "/galaxy/template/queryTemplate",
 	/**
 	 * 查询模板相关数据
 	 */
-	getTempRelatedData : sopContentUrl + "/galaxy/template/getRelatedData",
+	getTempRelatedData : Constants.sopEndpointURL + "/galaxy/template/getRelatedData",
 	/**
 	 * 模板上传
 	 */
-	tempUpload : sopContentUrl + "/galaxy/template/upload",
+	tempUpload : Constants.sopEndpointURL + "/galaxy/template/upload",
 	/**
 	 * 模板保存
 	 */
-	tempSave : sopContentUrl + "/galaxy/template/save",
+	tempSave : Constants.sopEndpointURL + "/galaxy/template/save",
 	/**
 	 * 模板下载
 	 */
-	tempDownload : sopContentUrl + "/galaxy/template/download",
+	tempDownload : Constants.sopEndpointURL + "/galaxy/template/download",
 	
 	/**
 	 * 修改团队成员
 	 */
-	updatePerson : sopContentUrl + "/galaxy/project/upp",
+	updatePerson : Constants.sopEndpointURL + "/galaxy/project/upp",
 	
 	/**
 	 * 立项排期所有
 	 */
-    moreProjectMeeting : sopContentUrl + "/galaxy/home/moreProjectMeeting",
+    moreProjectMeeting : Constants.sopEndpointURL + "/galaxy/home/moreProjectMeeting",
 	/**
 	 * top5立项会排期
 	 */
-	top5ProjectMeeting : sopContentUrl + "/galaxy/home/top5ProjectMeeting",
+	top5ProjectMeeting : Constants.sopEndpointURL + "/galaxy/home/top5ProjectMeeting",
 	/**
 	 * 投诀会排期top5
 	 */
-	ProjectVoteWill: sopContentUrl + "/galaxy/home/ProjectVoteWill",
+	ProjectVoteWill: Constants.sopEndpointURL + "/galaxy/home/ProjectVoteWill",
 	/**
 	 * 投诀会排期所有
 	 */
-	moreProjectVoteWill: sopContentUrl + "/galaxy/home/moreProjectVoteWill",
+	moreProjectVoteWill: Constants.sopEndpointURL + "/galaxy/home/moreProjectVoteWill",
 	/**
 	 * 紧急任务总数
 	 */
-	totalUrgent: sopContentUrl + "/galaxy/soptask/totalUrgent",
+	totalUrgent: Constants.sopEndpointURL + "/galaxy/soptask/totalUrgent",
 	/**
 	 * 待办任务总数
 	 */
-	totalMission: sopContentUrl + "/galaxy/soptask/totalMission",
+	totalMission: Constants.sopEndpointURL + "/galaxy/soptask/totalMission",
 	
 	
 	/**gxc
 	 * 主页的待办任务:
 	 */		
-	soptaskshouye: sopContentUrl+"/galaxy/soptask/taskListByRole",
+	soptaskshouye: Constants.sopEndpointURL+"/galaxy/soptask/taskListByRole",
 	
 	/**-------首页相关 end  */
 	
 	/**
 	 * 添加股权结构
 	 */
-	addStock : sopContentUrl + "/galaxy/projectShares/addProjectShares",
+	addStock : Constants.sopEndpointURL + "/galaxy/projectShares/addProjectShares",
 	
 	/**
 	 * 修改股权结构
 	 */
-	updateStock : sopContentUrl + "/galaxy/projectShares/updateProjectShares",
+	updateStock : Constants.sopEndpointURL + "/galaxy/projectShares/updateProjectShares",
 	
 	
 	/**
 	 * tab -> 内评会： 
 	 */
-	tab_lphtc : sopContentUrl + "/galaxy/project/lphtc",
+	tab_lphtc : Constants.sopEndpointURL + "/galaxy/project/lphtc",
 	
 	/**
 	 * tab -> ceo评审：  
 	 */
-	tab_ceotc : sopContentUrl + "/galaxy/project/ceopstc",
+	tab_ceotc : Constants.sopEndpointURL + "/galaxy/project/ceopstc",
 	
 	/**
 	 * tab -> 立项会 申请排期： 
 	 */
-	tab_lxhpq : sopContentUrl + "/galaxy/project/progress/proSchedule",
+	tab_lxhpq : Constants.sopEndpointURL + "/galaxy/project/progress/proSchedule",
 	/**
 	 * tab -> 立项会： 
 	 */
-	tab_lxhtc : sopContentUrl + "/galaxy/project/lxhtc",
+	tab_lxhtc : Constants.sopEndpointURL + "/galaxy/project/lxhtc",
 	/**
 	 * tab -> 投决会： 
 	 */
-	tab_tjhtc : sopContentUrl + "/galaxy/project/tjhtc",
+	tab_tjhtc : Constants.sopEndpointURL + "/galaxy/project/tjhtc",
 	/**
 	 * 修改:"是否涉及股权转让"状态
 	 */
-	storeUrl : sopContentUrl + "/galaxy/project/store/",
+	storeUrl : Constants.sopEndpointURL + "/galaxy/project/store/",
 	/**
 	 * 获取登录人的项目（会议类型、访谈阶段）：
 	 */
-	getUserPro : sopContentUrl + "/galaxy/project/progress/queryPerPro",
+	getUserPro : Constants.sopEndpointURL + "/galaxy/project/progress/queryPerPro",
 	
 	/**
 	 * 访谈添加：附件上传
 	 */
-	saveInteverView : sopContentUrl + "/galaxy/project/progress/addInterview",
+	saveInteverView : Constants.sopEndpointURL + "/galaxy/project/progress/addInterview",
 	/**
 	 * 访谈添加 弹出 page
 	 */
-	toViewAdd : sopContentUrl + "/galaxy/project/progress/interViewAdd",
+	toViewAdd : Constants.sopEndpointURL + "/galaxy/project/progress/interViewAdd",
 	/**
 	 * 访谈查询 page
 	 */
-	selectViewPage : sopContentUrl + "/galaxy/project/progress/queryInterview",
+	selectViewPage : Constants.sopEndpointURL + "/galaxy/project/progress/queryInterview",
 	/**
 	 * 访谈添加：
 	 */
-	saveView : sopContentUrl + "/galaxy/project/progress/addInterview",
+	saveView : Constants.sopEndpointURL + "/galaxy/project/progress/addInterview",
 	/**
 	 * 访谈添加,附加附件：
 	 */
-	saveViewFile : sopContentUrl + "/galaxy/project/progress/addFileInterview",
+	saveViewFile : Constants.sopEndpointURL + "/galaxy/project/progress/addFileInterview",
 	
 	/**
 	 * 会议添加 弹出 page
 	 */
-	tomeetAdd : sopContentUrl + "/galaxy/project/progress/meetAddView",
+	tomeetAdd : Constants.sopEndpointURL + "/galaxy/project/progress/meetAddView",
 	/**
 	 * 会议添加：
 	 */
-	saveMeet : sopContentUrl + "/galaxy/project/progress/addmeet",
+	saveMeet : Constants.sopEndpointURL + "/galaxy/project/progress/addmeet",
 	/**
 	 * 会议+附件添加：
 	 */
-	saveMeetFile : sopContentUrl + "/galaxy/project/progress/addfilemeet",
+	saveMeetFile : Constants.sopEndpointURL + "/galaxy/project/progress/addfilemeet",
 	/**
 	 * 会议查询：
 	 */
-	selectMeetPage : sopContentUrl + "/galaxy/project/progress/queryMeet",
+	selectMeetPage : Constants.sopEndpointURL + "/galaxy/project/progress/queryMeet",
 	/**
 	 * 根据findByParentCode查询数据字典
 	 */
-	dictFindByParentCode : sopContentUrl + "/galaxy/sopFile/getDictByParent",
+	dictFindByParentCode : Constants.sopEndpointURL + "/galaxy/sopFile/getDictByParent",
 	/**
 	 * 获取页面显示权限
 	 */
-	sopFileCheckShow : sopContentUrl + "/galaxy/sopFile/checkShow",
+	sopFileCheckShow : Constants.sopEndpointURL + "/galaxy/sopFile/checkShow",
 	/**
 	 * 文档查询-无分页
 	 */
-	queryFile : sopContentUrl+"/galaxy/sopFile/query",
+	queryFile : Constants.sopEndpointURL+"/galaxy/sopFile/query",
 	/**
 	 * 任务处理-上传文档
 	 */
-	uploadFile2Task : sopContentUrl+"/galaxy/taskprocess/uploadFile",
+	uploadFile2Task : Constants.sopEndpointURL+"/galaxy/taskprocess/uploadFile",
 	/**
 	 * 任务处理-上传签署凭证
 	 */
-	uploadVoucher2Task : sopContentUrl+"/galaxy/taskprocess/uploadVoucher",
+	uploadVoucher2Task : Constants.sopEndpointURL+"/galaxy/taskprocess/uploadVoucher",
 	/**
 	 * 档案列表查询(不分页)
 	 */
-	searchSopFileListWithoutPage : sopContentUrl + "/galaxy/sopFile/searchSopFileListWithoutPage",
+	searchSopFileListWithoutPage : Constants.sopEndpointURL + "/galaxy/sopFile/searchSopFileListWithoutPage",
 	/**
 	 * 档案上传
 	 */
-	simpleSopFileUpload : sopContentUrl + '/galaxy/sopFile/simpleUpload',
+	simpleSopFileUpload : Constants.sopEndpointURL + '/galaxy/sopFile/simpleUpload',
 	/**
 	 * 启动内部评审
 	 */
-	startReview : sopContentUrl + "/galaxy/project/startReview/",
+	startReview : Constants.sopEndpointURL + "/galaxy/project/startReview/",
 	/**
 	 * 我的项目-文档下载
 	 */
-	downLoadFile : sopContentUrl+"/galaxy/sopFile/downloadFile",
+	downLoadFile : Constants.sopEndpointURL+"/galaxy/sopFile/downloadFile",
 	/**
 	 * 获取尽职调查文件列表
 	 */
-	getFileList : sopContentUrl + "/galaxy/project/progress/proFileInfo/",
+	getFileList : Constants.sopEndpointURL + "/galaxy/project/progress/proFileInfo/",
 	/**
 	 * 更新任务状态
 	 */
-	updateTaskStatus: sopContentUrl + "/galaxy/soptask/updateTaskStatus",
+	updateTaskStatus: Constants.sopEndpointURL + "/galaxy/soptask/updateTaskStatus",
 	/**
 	 * 人法财提交完成
 	 */
-	submitTask: sopContentUrl + "/galaxy/soptask/submitTask",
+	submitTask: Constants.sopEndpointURL + "/galaxy/soptask/submitTask",
 	/**
 	 * 项目阶段过程的记录保存
 	 */
-	stageChange : sopContentUrl + "/galaxy/project/stageChange?sid="+sessionId + "&guid="+userId,
+	stageChange : Constants.sopEndpointURL + "/galaxy/project/stageChange?sid="+sessionId + "&guid="+userId,
 	/**
 	 * 申请立项会操作
 	 */
-	toEstablishStage : sopContentUrl + "/galaxy/project/ges/",
-	inLxmeetingPool : sopContentUrl + "/galaxy/project/inlx/",
+	toEstablishStage : Constants.sopEndpointURL + "/galaxy/project/ges/",
+	inLxmeetingPool : Constants.sopEndpointURL + "/galaxy/project/inlx/",
 	/**
 	 * 申请投决会
 	 */
-	inTjh : sopContentUrl + "/galaxy/project/smp/",
-	inSureMeetingPool : sopContentUrl + "/galaxy/project/intj/",
+	inTjh : Constants.sopEndpointURL + "/galaxy/project/smp/",
+	inSureMeetingPool : Constants.sopEndpointURL + "/galaxy/project/intj/",
 	/**
 	 * 我的日程
 	 */
-	toShedule : sopContentUrl + "/galaxy/sopUserSchedule/scheduleList",
+	toShedule : Constants.sopEndpointURL + "/galaxy/sopUserSchedule/scheduleList",
 	/**
 	 * 档案列表
 	 */
-	searchSopFileList : sopContentUrl + "/galaxy/sopFile/searchSopFileList",
+	searchSopFileList : Constants.sopEndpointURL + "/galaxy/sopFile/searchSopFileList",
 	/**
 	 * 项目查询
 	 */
-	searchProject : sopContentUrl + "/galaxy/project/spl",
+	searchProject : Constants.sopEndpointURL + "/galaxy/project/spl",
 	/**
 	 * 获取部门字典
 	 */
-	getDepartMentDict : sopContentUrl + "/galaxy/sopFile/getDepartmentDict",
+	getDepartMentDict : Constants.sopEndpointURL + "/galaxy/sopFile/getDepartmentDict",
 	/**
 	 * 档案上传通用
 	 */
-	commonUploadFile : sopContentUrl + "/galaxy/sopFile/commonUploadFile",
+	commonUploadFile : Constants.sopEndpointURL + "/galaxy/sopFile/commonUploadFile",
 	
-	showTask: sopContentUrl + "/galaxy/soptask",
+	showTask: Constants.sopEndpointURL + "/galaxy/soptask",
 	
-	operationMessageQueryList : sopContentUrl + "/galaxy/operationMessage/queryList",
+	operationMessageQueryList : Constants.sopEndpointURL + "/galaxy/operationMessage/queryList",
 	
-	operationMessageRemind : sopContentUrl + "/galaxy/operationMessage/remind",
-	MessageIndex : sopContentUrl + "/galaxy/operationMessage/index",
+	operationMessageRemind : Constants.sopEndpointURL + "/galaxy/operationMessage/remind",
+	MessageIndex : Constants.sopEndpointURL + "/galaxy/operationMessage/index",
 	
 	/***
 	 * 日程显示三条记录
 	 */
-	sheduleMoreThree : sopContentUrl + "/galaxy/sopUserSchedule/selectSopUserSchedule/1",
+	sheduleMoreThree : Constants.sopEndpointURL + "/galaxy/sopUserSchedule/selectSopUserSchedule/1",
 	
 	/***
 	 * 保存日程
 	 */
-	saveShedule : sopContentUrl + "/galaxy/sopUserSchedule/addOrUpdateSopUserSchedule/",
+	saveShedule : Constants.sopEndpointURL + "/galaxy/sopUserSchedule/addOrUpdateSopUserSchedule/",
 	
 	/**
 	 * 获取日程
 	 */
-	sheduleInfo : sopContentUrl + "/galaxy/sopUserSchedule/getSchedule/",
+	sheduleInfo : Constants.sopEndpointURL + "/galaxy/sopUserSchedule/getSchedule/",
 	
 	/**
 	 * 删除日程
 	 */
-	deleteShedule : sopContentUrl + "/galaxy/sopUserSchedule/delete/",
+	deleteShedule : Constants.sopEndpointURL + "/galaxy/sopUserSchedule/delete/",
 	
 	/**
 	 * 添加团队成员跳转页面
 	 */
-	addPersonView : sopContentUrl + "/galaxy/addperson",
+	addPersonView : Constants.sopEndpointURL + "/galaxy/addperson",
 	/**
 	 * 修改团队成员
 	 */
-	updatePerView : sopContentUrl + "/galaxy/project/updatePro/",
+	updatePerView : Constants.sopEndpointURL + "/galaxy/project/updatePro/",
 	
 	/***
 	 * 添加股权结构页面
 	 */
-	addSharesView : sopContentUrl + "/galaxy/projectShares/addShares",
+	addSharesView : Constants.sopEndpointURL + "/galaxy/projectShares/addShares",
 	
 	/**
 	 * 修改股权结构面板
 	 */
-	editStockView : sopContentUrl + "/galaxy/projectShares/updateShare/",
+	editStockView : Constants.sopEndpointURL + "/galaxy/projectShares/updateShare/",
 	/***
 	 * 获取股权结构列表
 	 */
-	projectSharesList : sopContentUrl + "/galaxy/projectShares/selectProjectShares",
+	projectSharesList : Constants.sopEndpointURL + "/galaxy/projectShares/selectProjectShares",
 	
 	/**
 	 * 获取团队成员列表
 	 */
-	projectPersonList : sopContentUrl + "/galaxy/project/queryProjectPerson",
+	projectPersonList : Constants.sopEndpointURL + "/galaxy/project/queryProjectPerson",
 	/**
 	 * 删除股权结构
 	 */
-	deleteProjectShares : sopContentUrl + "/galaxy/projectShares/deleteProjectShares/",
+	deleteProjectShares : Constants.sopEndpointURL + "/galaxy/projectShares/deleteProjectShares/",
 	
 	/***
 	 * 删除团队成员
 	 */
-	deletePPerson : sopContentUrl + "/galaxy/project/dpp/" ,
+	deletePPerson : Constants.sopEndpointURL + "/galaxy/project/dpp/" ,
 	
 	/**
 	 * 完善简历
 	 */
-	addPersonHr :  sopContentUrl + "/galaxy/hrjl/addPersonHr" ,
+	addPersonHr :  Constants.sopEndpointURL + "/galaxy/hrjl/addPersonHr" ,
 	
 	/**
 	 * 完善简历回显
 	 */
-	toaddPersonHr:sopContentUrl + "/galaxy/hrjl/toaddPersonHr/" ,
+	toaddPersonHr:Constants.sopEndpointURL + "/galaxy/hrjl/toaddPersonHr/" ,
 	/**
 	 * 跳到完善简历页面
 	 */
-	toWanshan: sopContentUrl + "/galaxy/project/queryPersonListToTask",
+	toWanshan: Constants.sopEndpointURL + "/galaxy/project/queryPersonListToTask",
 	/**
 	 * 跳到完善简历页面(弹出)
 	 */
-	personHr: sopContentUrl+"/galaxy/hrjl/resumetcc",
+	personHr: Constants.sopEndpointURL+"/galaxy/hrjl/resumetcc",
 	/**
 	 * 跳到完善简历之项目成员里的个人简历(弹出)
 	 */
 	
-	personHHr: sopContentUrl+"/galaxy/hrjl/genresumetcc",
+	personHHr: Constants.sopEndpointURL+"/galaxy/hrjl/genresumetcc",
 	
 	/**
 	 * 催办
 	 */
-	taskUrged:sopContentUrl + "/galaxy/taskprocess/taskUrged",
+	taskUrged:Constants.sopEndpointURL + "/galaxy/taskprocess/taskUrged",
 	
-	tempSendMail:sopContentUrl + "/galaxy/template/sendMail" ,
+	tempSendMail:Constants.sopEndpointURL + "/galaxy/template/sendMail" ,
 	/**
 	 * Ajax判断项目名称，组织机构代码是否重复
 	 */
-	checkProject:sopContentUrl + "/galaxy/project/checkProject",
+	checkProject:Constants.sopEndpointURL + "/galaxy/project/checkProject",
 	/**
 	 * 档案管理发送邮件窗口
 	 */
-	showFileMailDialog:sopContentUrl + "/galaxy/sopFile/showMailDialog",
+	showFileMailDialog:Constants.sopEndpointURL + "/galaxy/sopFile/showMailDialog",
 	/**
 	 * 档案管理发送邮
 	 */
-	fileSendEmail:sopContentUrl + "/galaxy/sopFile/sendMail",
-	
+	fileSendEmail:Constants.sopEndpointURL + "/galaxy/sopFile/sendMail",
 	/**
 	 * 排期池中是否存在
 	 */
-	checkHasPool:sopContentUrl + "/galaxy/project/checkHasPool",
+	checkHasPool:Constants.sopEndpointURL + "/galaxy/project/checkHasPool",
 	/**
 	 * 是否存在通过的会议
 	 */
-	checkPassMeet:sopContentUrl + "/galaxy/project/checkPassMeet",
-	
+	checkPassMeet:Constants.sopEndpointURL + "/galaxy/project/checkPassMeet",
 	/**
 	 * 是否存在通过的会议
 	 */
-	checkCanUse:sopContentUrl + "/galaxy/project/checkCanUse"
-	
-	
+
+	checkCanUse:Constants.sopEndpointURL + "/galaxy/project/checkCanUse",
+	/**
+	 * 获取阿里云签名
+	 */
+	getPolicy : Constants.sopEndpointURL + "/galaxy/sopFile/getPolicy",
+	/**
+	 * 阿里云直连回掉
+	 */
+	fileCallBack : Constants.sopEndpointURL + "/galaxy/sopFile/fileCallBack"
 }
 
 /**
