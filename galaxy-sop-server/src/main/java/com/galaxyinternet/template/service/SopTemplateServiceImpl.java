1ad88d003d139c44e2bb5404aafb6a703fcefcf4
swang <songwang@galaxyinternet.com>
2016-04-12 13:41:58 +0800

    wangosng 删除无效引用

--- galaxy-sop-server/src/main/java/com/galaxyinternet/template/service/SopTemplateServiceImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/template/service/SopTemplateServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.galaxyinternet.bo.template.SopTemplateBo;
 import com.galaxyinternet.dao.template.SopTemplateDao;
 import com.galaxyinternet.framework.core.dao.BaseDao;
 import com.galaxyinternet.framework.core.service.impl.BaseServiceImpl;
