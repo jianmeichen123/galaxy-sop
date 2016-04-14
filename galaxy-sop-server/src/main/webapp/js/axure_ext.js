557168b10db528910f8709bdf83c93f4a0c0058f
zhaoying <yingzhao@galaxyunternet.com>
2016-04-14 09:10:20 +0800

    --zhaoying--test

--- galaxy-sop-server/src/main/webapp/js/axure_ext.js
+++ galaxy-sop-server/src/main/webapp/js/axure_ext.js
@@ -365,7 +365,7 @@ $(function(){
 				$('.register_all_two1').html(getQueryString(_url,"realName"));
 				$('.register_all_two2').html(getQueryString(_url,"deptName"));
 				$('.register_all_two3').html(getQueryString(_url,"roleName"));
-				$("#hid").href=getUrl(_url);
+				//$("#hid").href=getUrl(_url);
 				
 				$("[data-btn='change_password']").on("click",function(){
 					$('#powindow').remove();
