c81cbf4ebc57f3569bc793b9ac4e9d1a1234e244
chenjianmei <jianmeichen@galaxyinternet.com>
2016-05-05 17:18:22 +0800

    update

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/bo/AbandonedBo.java
@@ -0,0 +1,13 @@
+package com.galaxyinternet.bo;
+
+import com.galaxyinternet.model.idea.Abandoned;
+
+public class AbandonedBo extends Abandoned  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+}
---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/model/idea/Abandoned.java
@@ -0,0 +1,12 @@
+package com.galaxyinternet.model.idea;
+
+import com.galaxyinternet.framework.core.model.PagableEntity;
+
+public class Abandoned extends PagableEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
