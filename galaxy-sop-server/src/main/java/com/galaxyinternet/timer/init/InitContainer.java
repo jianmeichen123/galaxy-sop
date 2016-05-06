cd3c4c1a94ec87c12b077f537906d33d5820e0c1
ysh <ysh@ysh-PC>
2016-05-06 17:56:05 +0800

    过会率

--- galaxy-sop-server/src/main/java/com/galaxyinternet/timer/init/InitContainer.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/timer/init/InitContainer.java
@@ -1,4 +1,4 @@
-package com.galaxyinternet.common.init;
+package com.galaxyinternet.timer.init;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,27 +6,28 @@ import java.net.URL;
 import com.galaxyinternet.timer.AutoThreadManager;
 import com.galaxyinternet.timer.test.TimerEntrance;
 
-public class InitServlet{
-  public void init() throws Exception {
-	  InputStream is = null;
+public class InitContainer {
+
+	public void init() throws Exception {
+		InputStream is = null;
 		try {
-			//读取定时线程的配置文件
+			// 读取定时线程的配置文件
 			TimerEntrance entrance = new TimerEntrance();
-			URL path = entrance.getClass().getResource("/autostart.xml");			
+			URL path = entrance.getClass().getResource("/autostart.xml");
 			is = path.openStream();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//解析配置的自动运行线程，并把它们全部放入AutoRunThreadManager的一个List集合中
+		// 解析配置的自动运行线程，并把它们全部放入AutoRunThreadManager的一个List集合中
 		AutoThreadManager autoMgr = new AutoThreadManager(is);
 		autoMgr.setName("AutoRunThreadManager");
-		//启动自动运行线程管理类
-      autoMgr.start();
-      try {
+		// 启动自动运行线程管理类
+		autoMgr.start();
+		try {
 			is.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-  }
+	}
 
 }
