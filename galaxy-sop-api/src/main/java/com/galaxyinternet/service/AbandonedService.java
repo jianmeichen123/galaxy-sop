0cb4f85b6e95940160d3e6f9fc65dc7ffdb555d1
chenjianmei <jianmeichen@galaxyinternet.com>
2016-05-12 10:44:40 +0800

    update

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/service/AbandonedService.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.service;
+
+import com.galaxyinternet.framework.core.service.BaseService;
+import com.galaxyinternet.model.idea.Abandoned;
+
+public interface AbandonedService extends BaseService<Abandoned> {
+	
+}
