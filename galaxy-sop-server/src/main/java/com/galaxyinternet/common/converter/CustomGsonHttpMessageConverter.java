5b5be482f7f2bb9b9ac537490857bac47eef0220
liuyaxin <yaxinliu@galaxyinternet>
2016-05-12 13:23:10 +0800

    解决日期转换测试

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/converter/CustomGsonHttpMessageConverter.java
@@ -0,0 +1,29 @@
+package com.galaxyinternet.common.converter;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class CustomGsonHttpMessageConverter extends GsonHttpMessageConverter {
+
+	public CustomGsonHttpMessageConverter() {
+        // Creates the json object which will manage the information received
+        GsonBuilder builder = new GsonBuilder();
+
+        // Register an adapter to manage the date types as long values
+        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+
+        setGson(builder.create());
+    }
+}
