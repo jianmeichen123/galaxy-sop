30a00834f51f2298da90a4de245c620f6a25eab7
liuyaxin <yaxinliu@galaxyinternet>
2016-05-12 13:28:14 +0800

    修改代码测试

--- galaxy-sop-server/src/main/java/com/galaxyinternet/common/converter/CustomGsonHttpMessageConverter.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/converter/CustomGsonHttpMessageConverter.java
@@ -1,6 +1,7 @@
 package com.galaxyinternet.common.converter;
 
 import java.lang.reflect.Type;
+import java.sql.Timestamp;
 import java.util.Date;
 
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
@@ -18,10 +19,15 @@ public class CustomGsonHttpMessageConverter extends GsonHttpMessageConverter {
         GsonBuilder builder = new GsonBuilder();
 
         // Register an adapter to manage the date types as long values
-        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+        /*builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
             public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
                 return new Date(json.getAsJsonPrimitive().getAsLong());
             }
+        });*/
+        builder.registerTypeAdapter(Timestamp.class, new JsonDeserializer<Timestamp>() {
+        	public Timestamp deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        		return new Timestamp(json.getAsJsonPrimitive().getAsLong());
+        	}
         });
 
         setGson(builder.create());
