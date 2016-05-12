a5b72c7d3b21083f07c7018b02b8b71a0d3e8f25
liuyaxin <yaxinliu@galaxyinternet>
2016-05-12 13:54:06 +0800

    修改实体类的get方法

--- galaxy-sop-api/src/main/java/com/galaxyinternet/model/project/MeetingScheduling.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/model/project/MeetingScheduling.java
@@ -13,71 +13,78 @@ import com.galaxyinternet.framework.core.utils.DateUtil;
 public class MeetingScheduling extends PagableEntity {
 	private static final long serialVersionUID = 1L;
 
-    private Long projectId;
+	private Long projectId;
 
-    private String meetingType;
+	private String meetingType;
 
-    private Integer meetingCount;
+	private Integer meetingCount;
 
-    private Date meetingDate;
+	private Date meetingDate;
 
-    private String status;
+	private String status;
 
-    private String remark;
-    private Integer scheduleStatus;
-    private Timestamp applyTime;
-    private Timestamp lastTime;
-    
-    private Timestamp reserveTimeStart;
-    private Timestamp reserveTimeEnd;
-    
-    //排序字段名称
-    private String sortName;
-    //排序方式
-    private String sortDirection;
-    //过滤字段
-    private String filterName;
-    
-    private  List<Long> deptIdList;
-    private List<Long> projectIdList;
-    //项目名称
-  	private String projectName;
-  	//投资事业线
-  	private String projectCareerline;
-  	//投资经理
-    private String createUname;
-    private String meetingDateStr; 
-    private String applyTimeStr;
-    
-    private String reserveTimeStartStr;
-    private String reserveTimeEndStr;
-    //投资事业线部门id
-    private Integer careline;
-    
-    
-    //申请时间(申请立项会时间)
-    private  Date meetTime;
-    
-    //项目代码
-    private String projectCode;
-    
-    //权限部分
-    private byte isEdit;
-    
-    
-    private Double meetingRate;
-    
-   
+	private String remark;
+	private Integer scheduleStatus;
+	private Timestamp applyTime;
+	private Timestamp lastTime;
+
+	private Timestamp reserveTimeStart;
+	private Timestamp reserveTimeEnd;
+
+	// 排序字段名称
+	private String sortName;
+	// 排序方式
+	private String sortDirection;
+	// 过滤字段
+	private String filterName;
+
+	private List<Long> deptIdList;
+	private List<Long> projectIdList;
+	// 项目名称
+	private String projectName;
+	// 投资事业线
+	private String projectCareerline;
+	// 投资经理
+	private String createUname;
+	private String meetingDateStr;
+	private String applyTimeStr;
+
+	private String reserveTimeStartStr;
+	private String reserveTimeEndStr;
+	// 投资事业线部门id
+	private Integer careline;
+
+	// 申请时间(申请立项会时间)
+	private Date meetTime;
+
+	// 项目代码
+	private String projectCode;
+
+	// 权限部分
+	private byte isEdit;
+
+	private Double meetingRate;
 
 	public Timestamp getReserveTimeEnd() {
+		if (StringUtils.isNotBlank(this.reserveTimeEndStr)) {
+			Date tmp;
+			try {
+				tmp = DateUtil
+						.convertStringToDateTimeForChina(this.reserveTimeEndStr);
+				return new Timestamp(tmp.getTime());
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
 		return reserveTimeEnd;
 	}
 
 	public void setReserveTimeEnd(Timestamp reserveTimeEnd) {
 		this.reserveTimeEnd = reserveTimeEnd;
-		if(reserveTimeEnd != null){
-			reserveTimeEndStr = DateUtil.convertDateToStringForChina(reserveTimeEnd);
-        }
+		if (reserveTimeEnd != null) {
+			reserveTimeEndStr = DateUtil
+					.convertDateToStringForChina(reserveTimeEnd);
+		}
 	}
 
 	public String getReserveTimeStartStr() throws ParseException {
@@ -86,15 +93,6 @@ public class MeetingScheduling extends PagableEntity {
 
 	public void setReserveTimeStartStr(String reserveTimeStartStr) {
 		this.reserveTimeStartStr = reserveTimeStartStr;
-		if(StringUtils.isNotBlank(this.reserveTimeStartStr)){
-			Date tmp = null;
-			try {
-				tmp = DateUtil.convertStringToDateTimeForChina(this.reserveTimeStartStr);
-				this.setReserveTimeStart(new Timestamp(tmp.getTime()));
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	public String getReserveTimeEndStr() {
@@ -103,15 +101,6 @@ public class MeetingScheduling extends PagableEntity {
 
 	public void setReserveTimeEndStr(String reserveTimeEndStr) {
 		this.reserveTimeEndStr = reserveTimeEndStr;
-		if(StringUtils.isNotBlank(this.reserveTimeEndStr)){
-			Date tmp;
-			try {
-				tmp = DateUtil.convertStringToDateTimeForChina(this.reserveTimeEndStr);
-				this.setReserveTimeStart(new Timestamp(tmp.getTime()));
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	public String getProjectCode() {
@@ -138,67 +127,66 @@ public class MeetingScheduling extends PagableEntity {
 		this.applyTimeStr = applyTimeStr;
 	}
 
-
 	public String getProjectCareerline() {
-  		return projectCareerline;
-  	}
+		return projectCareerline;
+	}
 
-  	public void setProjectCareerline(String projectCareerline) {
-  		this.projectCareerline = projectCareerline;
-  	}
+	public void setProjectCareerline(String projectCareerline) {
+		this.projectCareerline = projectCareerline;
+	}
 
-  	public String getCreateUname() {
-  		return createUname;
-  	}
+	public String getCreateUname() {
+		return createUname;
+	}
 
-  	public void setCreateUname(String createUname) {
-  		this.createUname = createUname;
-  	}
+	public void setCreateUname(String createUname) {
+		this.createUname = createUname;
+	}
 
-  	public String getProjectName() {
-  		return projectName;
-  	}
+	public String getProjectName() {
+		return projectName;
+	}
 
-  	public void setProjectName(String projectName) {
-  		this.projectName = projectName;
-  	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
 
-    public Long getProjectId() {
-        return projectId;
-    }
+	public Long getProjectId() {
+		return projectId;
+	}
 
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
 
-    public String getMeetingType() {
-        return meetingType;
-    }
+	public String getMeetingType() {
+		return meetingType;
+	}
 
-    public void setMeetingType(String meetingType) {
-        this.meetingType = meetingType == null ? null : meetingType.trim();
-    }
+	public void setMeetingType(String meetingType) {
+		this.meetingType = meetingType == null ? null : meetingType.trim();
+	}
 
-    public Integer getMeetingCount() {
-        return meetingCount;
-    }
+	public Integer getMeetingCount() {
+		return meetingCount;
+	}
 
-    public void setMeetingCount(Integer meetingCount) {
-        this.meetingCount = meetingCount;
-    }
+	public void setMeetingCount(Integer meetingCount) {
+		this.meetingCount = meetingCount;
+	}
 
-    public Date getMeetingDate() {
-        return meetingDate;
-    }
+	public Date getMeetingDate() {
+		return meetingDate;
+	}
 
-    public void setMeetingDate(Date meetingDate) {
-        this.meetingDate = meetingDate;
-        if(meetingDate != null){
-        	meetingDateStr = DateUtil.convertDateToStringForChina(meetingDate);
-        }
-    }
-    
-    public String getStatus() {
+	public void setMeetingDate(Date meetingDate) {
+		this.meetingDate = meetingDate;
+		if (meetingDate != null) {
+			meetingDateStr = DateUtil.convertDateToStringForChina(meetingDate);
+		}
+	}
+
+	public String getStatus() {
 		return status;
 	}
 
@@ -207,12 +195,12 @@ public class MeetingScheduling extends PagableEntity {
 	}
 
 	public String getRemark() {
-        return remark;
-    }
+		return remark;
+	}
 
-    public void setRemark(String remark) {
-        this.remark = remark == null ? null : remark.trim();
-    }
+	public void setRemark(String remark) {
+		this.remark = remark == null ? null : remark.trim();
+	}
 
 	public Integer getScheduleStatus() {
 		return scheduleStatus;
@@ -228,20 +216,31 @@ public class MeetingScheduling extends PagableEntity {
 
 	public void setApplyTime(Timestamp applyTime) {
 		this.applyTime = applyTime;
-		if(applyTime != null){
+		if (applyTime != null) {
 			applyTimeStr = DateUtil.convertDateToStringForChina(applyTime);
-        }
+		}
 	}
 
 	public Timestamp getReserveTimeStart() {
+		if (StringUtils.isNotBlank(this.reserveTimeStartStr)) {
+			Date tmp = null;
+			try {
+				tmp = DateUtil
+						.convertStringToDateTimeForChina(this.reserveTimeStartStr);
+				return new Timestamp(tmp.getTime());
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
 		return reserveTimeStart;
 	}
 
 	public void setReserveTimeStart(Timestamp reserveStartTime) {
 		this.reserveTimeStart = reserveStartTime;
-		if(reserveStartTime != null){
-			reserveTimeStartStr = DateUtil.convertDateToStringForChina(reserveTimeStart);
-        }
+		if (reserveStartTime != null) {
+			reserveTimeStartStr = DateUtil
+					.convertDateToStringForChina(reserveTimeStart);
+		}
 	}
 
 	public Timestamp getLastTime() {
@@ -299,9 +298,11 @@ public class MeetingScheduling extends PagableEntity {
 	public void setMeetTime(Date meetTime) {
 		this.meetTime = meetTime;
 	}
+
 	public byte getIsEdit() {
 		return isEdit;
 	}
+
 	public void setIsEdit(byte isEdit) {
 		this.isEdit = isEdit;
 	}
@@ -321,6 +322,5 @@ public class MeetingScheduling extends PagableEntity {
 	public void setMeetingRate(Double meetingRate) {
 		this.meetingRate = meetingRate;
 	}
-	
-	
+
 }
