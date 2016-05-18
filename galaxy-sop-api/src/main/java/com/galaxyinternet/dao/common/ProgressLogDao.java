52121c619afaee568e27aa289877a0ad4910c54a
swang <songwang@galaxyinternet.com>
2016-05-18 15:08:20 +0800

    wangosng 创意/项目动态接口

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/common/ProgressLogDao.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.dao.common;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.common.ProgressLog;
+
+public interface ProgressLogDao extends BaseDao<ProgressLog, Long> {
+
+}
---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/progresslog/ProgressLogServiceImpl.java
@@ -0,0 +1,19 @@
+package com.galaxyinternet.progresslog;
+
+import org.springframework.stereotype.Service;
+
+import com.galaxyinternet.dao.common.ProgressLogDao;
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.framework.core.service.impl.BaseServiceImpl;
+import com.galaxyinternet.model.common.ProgressLog;
+import com.galaxyinternet.service.ProgressLogService;
+@Service
+public class ProgressLogServiceImpl extends BaseServiceImpl<ProgressLog>implements ProgressLogService {
+
+	private ProgressLogDao progressLogDao;
+	@Override
+	protected BaseDao<ProgressLog, Long> getBaseDao() {
+		return progressLogDao;
+	}
+
+}
