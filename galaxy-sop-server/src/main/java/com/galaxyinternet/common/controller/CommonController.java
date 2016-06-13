2e8bbf2efe4ad15fcbaa5708821e49ade8c68f3b
Administrator <Administrator@EZ-20160505NBWT>
2016-06-13 10:41:52 +0800

    ignore

--- galaxy-sop-server/src/main/java/com/galaxyinternet/common/controller/CommonController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/controller/CommonController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
