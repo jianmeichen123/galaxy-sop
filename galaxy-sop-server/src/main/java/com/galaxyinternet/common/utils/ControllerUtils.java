0e51db32eb2ef5eb8e02acf35a903824a484ca73
zhangfeng <fengzhang@falaxyinternet.com>
2016-06-28 10:28:11 +0800

    3333

--- galaxy-sop-server/src/main/java/com/galaxyinternet/common/utils/ControllerUtils.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/utils/ControllerUtils.java
@@ -14,13 +14,7 @@ public class ControllerUtils {
 	
 	
 	public static String getProjectNameLink(OperationMessage message) {
-		StringBuffer link = new StringBuffer();
-		link.append("<a href=\"#\" class=\"blue project_name\" data-project-id=\"")
-		.append(message.getProjectId())
-		.append("\">")
-		.append(message.getProjectName())
-		.append("</a>");
-		return link.toString();
+		return "projectname";
 	}
 
 	/**
