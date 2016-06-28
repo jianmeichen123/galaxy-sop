772066cf514a4be121c3c3fd6f0303166a4a5bef
lei <chujinglei@galaxyinternet.com>
2016-06-28 10:58:13 +0800

    update

--- galaxy-sop-server/src/main/webapp/plugins/daterangepicker/daterangepicker-customer.js
+++ galaxy-sop-server/src/main/webapp/plugins/daterangepicker/daterangepicker-customer.js
@@ -1217,12 +1217,17 @@
             var col = title.substr(3, 1);
             var cal = $(e.target).parents('.calendar');
             var date = cal.hasClass('left') ? this.leftCalendar.calendar[row][col] : this.rightCalendar.calendar[row][col];
-
-            if (this.endDate) {
+            var position = cal.hasClass('left') ? 'left' : 'right';
+            if(position=="left"){
+            	this.container.find('input[name=daterangepicker_start]').val(date.format(this.locale.format));
+            }else{
+            	this.container.find('input[name=daterangepicker_end]').val(date.format(this.locale.format));
+            }
+          /*  if (this.endDate) {
                 this.container.find('input[name=daterangepicker_start]').val(date.format(this.locale.format));
             } else {
                 this.container.find('input[name=daterangepicker_end]').val(date.format(this.locale.format));
-            }
+            }*/
 
             //highlight the dates between the start date and the date being hovered as a potential end date
             var leftCalendar = this.leftCalendar;
