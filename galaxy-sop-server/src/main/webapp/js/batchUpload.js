151bee2dd3d1522b35c05009d437fee1ca7a68e0
liuyaxin <yaxinliu@galaxyinternet.com>
2016-07-13 13:48:03 +0800

    tag:639bug修改

--- galaxy-sop-server/src/main/webapp/js/batchUpload.js
+++ galaxy-sop-server/src/main/webapp/js/batchUpload.js
@@ -57,6 +57,16 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 								}
 							});
 					}else{
+						/**
+						 * 最多只能上传10个文件
+						 */
+						var max_files = 10;
+	                    if (up.files.length > max_files) {
+	                    	//uploader.removeFile(file);
+	                    	$(".pop").hideLoading();
+	                    	layer.msg("最多只能传10个文件!");
+							return;
+						}
 						up.settings.multipart_params = params;
 						uploader.start();
 					}
@@ -78,15 +88,7 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 				}
              }, 
 			FilesAdded: function(up, files) {
-				var max_files = 10;
 				plupload.each(files, function(file) {
-					/**
-					 * 最多只能上传10个文件
-					 */
-                    if (up.files.length > max_files) {
-                    	uploader.removeFile(file);
-						return;
-					}
                     /**
                      * 生成上传文件的列表
                      */
@@ -146,10 +148,16 @@ function del(id,name,fieInputId){
     $("#"+id+"tr").remove();
 }
 
+/**
+ * 文件重命名
+ * @param file
+ * @param fileList
+ * @returns {String}
+ */
 function countSameSubFile(file,fileList){
 	var name = file.name.replace(/\s+/g,"");
-	var  subname = name.substring(0,name.lastIndexOf("."));
-	 var fix =  name.substring(name.lastIndexOf(".")+1,name.length);
+	var subname = name.substring(0,name.lastIndexOf("."));
+	var fix =  name.substring(name.lastIndexOf(".")+1,name.length);
 	var count = 0;
 	$("#"+fileList+" tr:gt(0)").each(function(){
 	  var inputValue = $(this).find("td").eq(0).text();
