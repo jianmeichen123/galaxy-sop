b5c2e67addb7c3283973b20e0cf11c625bff7293
liuyaxin <yaxinliu@galaxyinternet.com>
2016-08-25 11:11:58 +0800

    tag:--------

--- galaxy-sop-server/src/main/webapp/js/batchUpload.js
+++ galaxy-sop-server/src/main/webapp/js/batchUpload.js
@@ -42,10 +42,10 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 							 {
 							    'addClass': 'loading-indicator'						
 							 });
-					layer.load(1);
+					//layer.load(1);
 					if(up.files.length == 0){
 							sendPostRequestByJsonObj(sendFileUrl,params,function(data){
-								layer.closeAll('loading');
+								//layer.closeAll('loading');
 								$(".pop").hideLoading();
 								var result = data.result.status;
 								if(result == "OK"){
@@ -59,7 +59,7 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 								}
 							});
 					}else{
-						layer.closeAll('loading');
+						//layer.closeAll('loading');
 						up.settings.multipart_params = params;
 						uploader.start();
 					}
@@ -79,7 +79,7 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 					$("#"+file.id+"_progress").html('<span>'+"上传失败!"+"</span>");
 					return false;
 				}
-				layer.closeAll('loading');
+				//layer.closeAll('loading');
              }, 
 			FilesAdded: function(up, files) {
 				var max_files = 10;
@@ -119,7 +119,6 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 				if($("#"+fieInputId).val().trim()){
 					sendPostRequestByJsonObj(sendFileUrl,params,function(data){
 					var result = data.result.status;
-					$(".pop").hideLoading();
 					if(result == "OK"){
 						$.each(files, function(i) {     
 						    $("#"+files[i].id+"_progress").html('<span>'+ files[i].percent + "%</span>"); 
@@ -134,7 +133,8 @@ function toBachUpload(fileurl,sendFileUrl,fieInputId,selectBtnId,submitBtnId,con
 						    $("#"+files[i].id+"_progress").html('<span>'+"上传失败!"+"</span>"); 
 						}); 
 					    $("#"+fieInputId).val('');
-					    layer.closeAll('loading');
+					    //layer.closeAll('loading');
+					    $(".pop").hideLoading();
 					    alert(data.result.message);
 					}
 					
