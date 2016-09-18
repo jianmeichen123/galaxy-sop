1cba9430091956673661443a5dbe22e35fa12311
lei <chujinglei@galaxyinternet.com>
2016-09-18 14:25:00 +0800

    update

--- galaxy-sop-server/src/main/webapp/js/echarts_ytxm.js
+++ galaxy-sop-server/src/main/webapp/js/echarts_ytxm.js
@@ -1,7 +1,7 @@
     // 路径配置
         require.config({
             paths: {
-                echarts: 'http://echarts.baidu.com/build/dist'
+                echarts: '/sop/js'
             }
         });
         
