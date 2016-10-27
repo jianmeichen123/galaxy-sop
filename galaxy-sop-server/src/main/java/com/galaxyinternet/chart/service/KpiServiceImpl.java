ac67036b5180b9b5ef19b1ff44b61c9557c5606b
liuyaxin <yaxinliu@galaxyinternet.com>
2016-10-27 14:35:02 +0800

    0

--- galaxy-sop-server/src/main/java/com/galaxyinternet/chart/service/KpiServiceImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/chart/service/KpiServiceImpl.java
@@ -760,7 +760,7 @@ public class KpiServiceImpl extends BaseServiceImpl<ChartDataBo>implements KpiSe
 		//统计个人在部门内排名
 		Map<Long, Integer> depIdNuntMap = new HashMap<Long, Integer>();                   
 		for(Project pro : proList){
-			int ordNum = query.getPageNum() * query.getPageSize() + 1;
+			int ordNum = 1;
 			if(depIdNuntMap.containsKey(pro.getProjectDepartid())){
 				ordNum = depIdNuntMap.get(pro.getProjectDepartid())+1;
 				pro.setDepNumOrder(ordNum);
@@ -824,7 +824,7 @@ public class KpiServiceImpl extends BaseServiceImpl<ChartDataBo>implements KpiSe
 			userTjhPNum.put(record.getCreateUid(), record.getPassMeetNum());
 		}
 		
-		int companyRank = 0;
+		int companyRank = query.getPageNum() * query.getPageSize();
 		for(Project pro : proList){
 			companyRank += 1;
 			User user = uIdUserMap.get(pro.getCreateUid());
