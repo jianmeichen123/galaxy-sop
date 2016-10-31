925c5828bba22999b51850283ce5b75ad2c6f4ef
lei <chujinglei@galaxyinternet.com>
2016-10-31 21:06:06 +0800

    1

--- galaxy-sop-server/src/main/webapp/bootstrap/bootstrap-datepicker/js/rangeDateForWeek.js
+++ galaxy-sop-server/src/main/webapp/bootstrap/bootstrap-datepicker/js/rangeDateForWeek.js
@@ -228,10 +228,10 @@ function setDateRange(ev,startOrEnd){
 		    		 if(startOrEnd == "INIT"){
 		    			 
 		    			//获得本周的开始日期
-						    getWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek -7 -1);
+						    getWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek  -1);
 						    getWeekStartDate =  formatDate(getWeekStartDate);
 						    //获得本周的结束日期
-						    getWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek -7 -1));
+						    getWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek  -1));
 						    getWeekEndDate =  formatDate(getWeekEndDate);
 		    			
 			    		}else{
