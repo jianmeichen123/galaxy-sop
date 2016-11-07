80e08a0917657b609d59561a2c65c8b2c6c17f99
chenjianmei <jianmeichen@galaxyinternet.com>
2016-11-07 16:21:52 +0800

    11111111111111111

---
+++ galaxy-sop-server/src/main/webapp/js/v_add_project_history.js
@@ -0,0 +1,42 @@
+function addFinanceHistory(){
+	
+	var flagId=$("#flagId").val();
+	if(flagId==""){
+		flagId=null;
+	}
+	var nowFormData = $("#add_Historyform").serializeObject();
+	     sendPostRequestByJsonStr(platformUrl.saveFinanceHistory+"/"+flagId, nowFormData, function(data){
+			var re=data;
+			$("#flagId").val(data.entity.id);
+			$.popupOneClose();
+			formatterTable(re.entity.fh);
+		});
+	     return true;
+}
+function formatterTable(entity){
+	var html;
+	if(null==entity){
+		html="<tr><td rowspan='7' align='center'>暂无数据<td></tr>";
+	}else{
+		for(var i=0;i<entity.length;i++){
+			var obj=entity[i];
+			html=
+				"<tr>"+
+					"<td>"+obj.financeDate+"</td>"+
+					"<td>"+obj.financeFrom+"</td>"+
+					"<td>"+obj.financeAmount+"</td>"+
+					"<td>"+obj.financeUnit+"</td>"+
+					"<td>"+obj.financeProportion+"</td>"+
+					"<td>"+obj.financeStatus+"</td>"+
+					"<td>"+
+						"<a class='meet_edit blue' href='javascript:void(0)'>编辑</a>"+
+						"<a class='meet_delete blue' href='javascript:void(0)'>删除</a>"+
+					"</td>"+
+			   "</tr>";
+		}
+		
+	}
+	
+	$("#financeHistory_table").append(html);
+	
+}
