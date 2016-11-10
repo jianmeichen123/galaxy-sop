8b14c8c58c0d9565e21993b1b5e479b4f624458b
lei <chujinglei@galaxyinternet.com>
2016-11-10 18:36:38 +0800

    update

--- galaxy-sop-server/src/main/webapp/js/v_add_project_2.js
+++ galaxy-sop-server/src/main/webapp/js/v_add_project_2.js
@@ -1,11 +1,5 @@
 $(function(){
 	/**
-	 * 编辑弹出富文本窗口宽度设置
-	 */
-	var width_fwb=$('.tabtable_con_on').width();
-	$('.width_fwb').css('width',(width_fwb-20));
-	
-	/**
 	 * 富文本实例化
 	 */
 	var describeUm = UM.getEditor('describe_editor');//项目 描述
@@ -33,14 +27,31 @@ $(function(){
 		{
 			return;
 		}
+		 var scroll_top=$(this).offset().top;
+		 $('html,body').animate({  
+		        scrollTop: scroll_top
+		    }, 1000);  
 		var open=$(this).attr('data-name')
 		$('.'+open+'_on').show();
-		$('.'+open+'_on').css("top","30%")
+		//$('.'+open+'_on').css("top","30%")
 		$('.'+open+'_center').hide();
 		$('.bj_hui_on').show();
 		responseData();
+		//调整富文本宽度
+		var width=$(".addProject").width();
+		$("#step2 .edui-container").css("width",width*0.98);
+		$("#step2 .width_fwb").css("width",width*0.96);
+		//跳跃填写，提示框展示
+		/*var btnName=$(this).attr("data-btn");
+		btnNum=btnName.substring(btnName.length-1,btnName.length);
+		for(var i=0;i<btnNum;i++){
+			if($('[data-btn="editor'+i+'"]').text()=="" || $.trim($('[data-btn="editor'+i+'"]').text())==''){
+				var id=$('[data-btn="editor'+i+'"]').attr("id").replace("_show","_valiate");
+				$("#"+id).show();
+			}
+		}*/
+		
 	})
-	
 	/**
 	 * 富文本弹出框点击取消事件
 	 */
@@ -50,6 +61,19 @@ $(function(){
 		$('.'+close+'_center').show();
 		$('.bj_hui_on').hide();
 	})
+	/**
+	 * 取消
+	 */
+	$(".fffbtn").click(function(){
+		var idArr=$(this).siblings().attr("id").split("_");
+		id=idArr[idArr.length-1];
+		if($('#'+id+'_show').text()==""|| $.trim($('#'+id+'_show').text())==""){
+			$('#'+id+'_valiate').show();
+		}else{
+			$('#'+id+'_valiate').hide();
+		}
+		
+	})
 	
 	/**
 	 * 保存项目描述
@@ -58,7 +82,8 @@ $(function(){
 		var projectDescribe = describeUm.getContent();
 		var projectDescribeFinancing = describeUm2.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "projectDescribe" : projectDescribe,"projectDescribeFinancing":projectDescribeFinancing}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("describe_show");
 	});
 	
 	/**
@@ -67,7 +92,8 @@ $(function(){
 	$("#save_location").click(function(){
 		var companyLocation = companyUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "companyLocation" : companyLocation}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("location_show")
 	});
 	
 	/**
@@ -76,7 +102,8 @@ $(function(){
 	$("#save_portrait").click(function(){
 		var userPortrait = portraitUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "userPortrait" : userPortrait}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("portrait_show")
 	});
 	
 	/**
@@ -85,7 +112,8 @@ $(function(){
 	$("#save_business").click(function(){
 		var projectBusinessModel = businessUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "projectBusinessModel" : projectBusinessModel}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("business_show")
 	});
 	
 	/**
@@ -94,7 +122,7 @@ $(function(){
 	$("#save_operation").click(function(){
 		var operationalData = operationUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "operationalData" : operationalData}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
 	});
 	
 	/**
@@ -103,7 +131,8 @@ $(function(){
 	$("#save_industry").click(function(){
 		var industryAnalysis = industryUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "industryAnalysis" : industryAnalysis}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("industry_show")
 	});
 	
 	/**
@@ -112,7 +141,8 @@ $(function(){
 	$("#save_analysis").click(function(){
 		var prospectAnalysis = analysisUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "prospectAnalysis" : prospectAnalysis}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
+		Valiate("analysis_show")
 	});
 	
 	/**
@@ -121,7 +151,7 @@ $(function(){
 	$("#save_next_financing").click(function(){
 		var nextFinancingSource = nextFinancingUm.getContent();
 		sendPostRequestByJsonObj(Constants.sopEndpointURL+"galaxy/project/addProjectStep2", {"id" : pid, "nextFinancingSource" : nextFinancingSource}, saveCallBack);
-		step2Valiate("step2");
+		//step2Valiate("step2");
 	});
 	
 	/**
@@ -155,6 +185,47 @@ $(function(){
 			$("#analysis_show").html(data.entity.prospectAnalysis);
 			$("#next_financing_source_show").html(data.entity.nextFinancingSource);
 			
+			if($("#describe_show").html()!=""  && $.trim($("#describe_show").text())!="" ){
+				$(".describe_show").show();
+			}else{
+				$(".describe_show").hide();
+			};
+			if($("#location_show").html()!="" && $.trim($("#location_show").text())!=""){
+				$(".location_show").show();
+			}else{
+				$(".location_show").hide();
+			};
+			if($("#portrait_show").html()!="" && $.trim($("#portrait_show").text())!=""){
+				$(".portrait_show").show();
+			}else{
+				$(".portrait_show").hide();
+			};
+			if($("#business_model_show").html()!="" && $.trim($("#business_model_show").text())!=""){
+				$(".business_model_show").show();
+			}else{
+				$(".business_model_show").hide();
+			};
+			if($("#operational_data_show").html()!="" && $.trim($("#operational_data_show").text())!=""){
+				$(".operational_data_show").show();
+			}else{
+				$(".operational_data_show").hide();
+			};
+			if($("#industry_analysis_show").html()!="" && $.trim($("#industry_analysis_show").text())!=""){
+				$(".industry_analysis_show").show();
+			}else{
+				$(".industry_analysis_show").hide();
+			};
+			if($("#analysis_show").html()!="" && $.trim($("#analysis_show").text())!=""){
+				$(".analysis_show").show();
+			}else{
+				$(".analysis_show").hide();
+			};
+			if($("#next_financing_source_show").html()!="" && $.trim($("#next_financing_source_show").text())!=""){
+				$(".next_financing_source_show").show();
+			}else{
+				$(".next_financing_source_show").hide();
+			};
+			
 		}else{
 			layer.msg(data.result.message);
 		}
@@ -170,7 +241,28 @@ function step2Valiate(id){
 		 if($(n).attr("valiate")=='required') {//对不能为空的文本框进行验证
 			 var id = $(n).attr("id").replace("_show","_valiate");
 			 if($(n).text()=='' || $.trim($(n).text())=='') {
-				 $("#"+id).html('<span style="font-color:red">参数丢失!</span>');
+				/* $("#"+id).html('<span style="font-color:red">参数丢失!</span>');*/
+				 $("#"+id).show();
+				 flag = false;
+			 }else{
+				 $("#"+id).hide();
+			 }
+			 
+		 }
+	});
+	return flag;
+}
+/**
+ * 表单单个验证
+ */
+function Valiate(id){
+	var flag = true;
+	$.each($("#"+id),function(i, n) {
+		 //清除可能已有的提示信息
+		 if($(n).attr("valiate")=='required') {//对不能为空的文本框进行验证
+			 var id = $(n).attr("id").replace("_show","_valiate");
+			 if($(n).text()=='' || $.trim($(n).text())=='') {
+				/* $("#"+id).html('<span style="font-color:red">参数丢失!</span>');*/
 				 $("#"+id).show();
 				 flag = false;
 			 }else{
