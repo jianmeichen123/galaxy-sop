964763cd6cec0584a87b7c053dfb4a1b518033b9
lei <chujinglei@galaxyinternet.com>
2016-11-21 09:34:10 +0800

    update

--- galaxy-sop-server/src/main/webapp/js/v_baseInfo_project_history.js
+++ galaxy-sop-server/src/main/webapp/js/v_baseInfo_project_history.js
@@ -73,7 +73,7 @@ function toUpdateOrSave(id){
 		data:"",//传递参数
 		okback:function(){
 			console.log(id);
-			if(null==id&&"underfined"==id){
+			if(null==id&& undefined==id){
 				$("#popup_name").text("添加融资历史");
 				/**
 				 * 获取融资状态下拉项
