bdff2882a8d2f8b52170ffccb204eea8a3259144
swang <songwang@galaxyinternet.com>
2017-03-13 19:06:34 +0800

    Info controller

--- galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/controller/InfoProjectController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/controller/InfoProjectController.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.galaxyinternet.common.controller.BaseControllerImpl;
+import com.galaxyinternet.framework.core.model.ResponseData;
 import com.galaxyinternet.framework.core.service.BaseService;
 import com.galaxyinternet.model.hologram.InformationData;
 import com.galaxyinternet.service.hologram.InformationDataService;
@@ -26,6 +29,24 @@ public class InfoProjectController  extends BaseControllerImpl<InformationData, 
 	@Override
 	protected BaseService<InformationData> getBaseService() {
 		return this.infoDataService;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/save")
+	public ResponseData<InformationData> save(@RequestBody InformationData data)
+	{
+		ResponseData<InformationData> rtn = new ResponseData<>();
+		
+		try
+		{
+			infoDataService.save(data);
+		} catch (Exception e)
+		{
+			logger.error("保存失败，信息:"+data,e);
+			rtn.getResult().addError("保存失败");
+		}
+		
+		return rtn;
 	}
 	
 }
