9e45577a39a92935bfadf648285864fca1435dba
chenjianmei <jianmeichen@galaxyinternet.com>
2017-03-13 18:36:06 +0800

    update

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/controller/InfoDateController.java
@@ -0,0 +1,35 @@
+package com.galaxyinternet.hologram.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.galaxyinternet.common.controller.BaseControllerImpl;
+import com.galaxyinternet.framework.core.service.BaseService;
+import com.galaxyinternet.model.hologram.InformationData;
+import com.galaxyinternet.project.controller.ProjectProgressController;
+import com.galaxyinternet.service.hologram.InformationDataService;
+
+
+@Controller
+@RequestMapping("/galaxy/infomation")
+public class InfoDateController  extends BaseControllerImpl<InformationData, InformationData> {
+
+	final Logger logger = LoggerFactory.getLogger(ProjectProgressController.class);
+	
+	
+	@Autowired
+	private InformationDataService infoDataService;
+
+	@Override
+	protected BaseService<InformationData> getBaseService() {
+		return this.infoDataService;
+	}
+	
+}
+
+
+
