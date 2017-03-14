3b03991412b52ce6fb7cfd11527f291eb305b04e
chenjianmei <jianmeichen@galaxyinternet.com>
2017-03-14 15:02:27 +0800

    update

--- galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/controller/JumpController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/controller/JumpController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -92,7 +91,7 @@ public class JumpController{
 	}
 	
 	/**
-	 * 投后运营，战略以及策略
+	 * 全息图-战略以及策略
 	 * @param request
 	 * @return
 	 */
