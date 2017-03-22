e81fd706fea65425d925f81b21f7ef7143323672
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-03-22 11:46:27 +0800

    zcy

---
+++ galaxy-sop-server/src/main/webapp/js/hologram/team.js
@@ -0,0 +1,9 @@
+
+alert("team.js!")
+showlist();
+function showlist(){
+   var tabel= $("#team");
+   sendGetRequest("http://fx.local.galaxyinternet.com/sop/galaxy/infoProject/getTitleResults/1302/1",null,function(data){
+       tabel.
+   })
+}
