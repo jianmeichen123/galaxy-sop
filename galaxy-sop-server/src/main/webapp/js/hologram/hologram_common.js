329ff34740eca889176b4ac847a8204a48b0b3fe
liulijun <11141736405@qq.com>
2017-04-06 09:39:45 +0800

    verify_32 正则修改

--- galaxy-sop-server/src/main/webapp/js/hologram/hologram_common.js
+++ galaxy-sop-server/src/main/webapp/js/hologram/hologram_common.js
@@ -791,8 +791,7 @@ jQuery.validator.addMethod("vinputValRule_3", function(value, element) {
 }, "不能超过100"); 
 //inputValRuleMark=="3,2"
 jQuery.validator.addMethod("verify_32", function(value, element) {   
-	//var verify_32 = /^(\d|[1-9]\d|100)(\.\d{1,2})?$/;
-	var verify_32 = /^((\d|[123456789]\d)(\.\d{1,2})?|100|100.0|100.00)$/;
+	var verify_32 = /^(\d|[1-9]\d?(\.\d{1,2})?|0\.\d{1,2}|100|100\.\d{1,2})$/;
 	return this.optional(element) || (verify_32.test(value));
 }, "不能超过100"); 
 //inputValRuleMark=="5,2"
