25e0547255a98a74a239c9dbb74c9ca370a12ddf
liuyaxin <yaxinliu@galaxyinternet.com>
2017-04-06 16:24:13 +0800

    tag:update

--- galaxy-sop-server/src/main/webapp/js/hologram/hologram_common.js
+++ galaxy-sop-server/src/main/webapp/js/hologram/hologram_common.js
@@ -574,9 +574,7 @@ function picData(pid){
 	var infoFileids = "";
 	var data={};
 	for(var i = 0;i < fileids.length; i++) {
-		  if(fileids.html().indexOf("img") > 0){
-			  fileids.html("");
-		  }
+		  fileids.html("未添加");
 		  infoFileids += ","+fileids.eq(i).attr("id").replace("look-","");
 	}
 	data.projectId = pid;
