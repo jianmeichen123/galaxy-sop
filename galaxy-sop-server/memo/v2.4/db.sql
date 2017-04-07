b53e7911ec02d19f8bbe1ddc9263fd6530468935
chenjianmei <jianmeichen@galaxyinternet.com>
2017-04-07 11:18:07 +0800

    update

---
+++ galaxy-sop-server/memo/v2.4/db.sql
@@ -0,0 +1,6 @@
+UPDATE `fx_db`.`t_department` SET `manager_id`='43' WHERE `id`='5';
+UPDATE `fx_db`.`t_department` SET `manager_id`='234' WHERE `id`='9';
+UPDATE `fx_db`.`t_department` SET `manager_id`='163' WHERE `id`='12';
+UPDATE `fx_db`.`t_department` SET `manager_id`='181' WHERE `id`='26';
+UPDATE `fx_db`.`t_department` SET `manager_id`='81' WHERE `id`='28';
+UPDATE `fx_db`.`t_department` SET `manager_id`='163' WHERE `id`='3006';
