fcaa57bc16e43f8b75cbd8936c00a0737c103a00
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-05-11 14:18:51 +0800

    update

--- galaxy-sop-server/memo/v1.4/拜访.sql
+++ galaxy-sop-server/memo/v1.4/拜访.sql
@@ -5,3 +5,8 @@ ADD COLUMN `significance` TINYINT(4) NULL COMMENT '拜访计划重要性：1 高
 /*访谈记录表增加 拜访计划ID*/
 ALTER TABLE `fx_db`.`sop_interview_record` 
 ADD COLUMN `schedule_id` BIGINT(20) NULL DEFAULT 0 COMMENT '关联拜访计划ID' AFTER `update_time`;
+
+UPDATE `fx_db`.`schedule_info` SET `project_type`='0' WHERE project_type is null;
+
+UPDATE `fx_db`.`schedule_info` SET `project_id`='0' WHERE project_id is null;
+
