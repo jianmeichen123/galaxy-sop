09651eac237c5d22d10669f65f224d9860468e60
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-05-12 11:04:39 +0800

    update

--- galaxy-sop-server/memo/v1.4/拜访.sql
+++ galaxy-sop-server/memo/v1.4/拜访.sql
@@ -10,7 +10,7 @@ UPDATE `fx_db`.`schedule_info` SET `project_type`='0' WHERE project_type is null
 
 UPDATE `fx_db`.`schedule_info` SET `project_id`='0' WHERE project_id is null;
 ALTER TABLE `fx_db`.`schedule_info` 
-ADD COLUMN `callonAddress` VARCHAR(200) NULL COMMENT '拜访地址';
+ADD COLUMN `callon_address` VARCHAR(200) NULL COMMENT '拜访地址';
 
 ALTER TABLE `fx_db`.`schedule_info` 
 ADD COLUMN `is_del` TINYINT(4) NULL DEFAULT '0' COMMENT '是否删除字段0:未删除,1:已删除' ;
