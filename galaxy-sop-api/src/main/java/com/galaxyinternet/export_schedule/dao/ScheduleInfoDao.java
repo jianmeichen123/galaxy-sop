f48140842aa24c44f2570df77fdd9cb20ad23299
liuyaxin <yaxinliu@galaxyinternet.com>
2017-05-18 13:38:11 +0800

    tag:commint

---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/export_schedule/dao/ScheduleInfoDao.java
@@ -0,0 +1,9 @@
+package com.galaxyinternet.export_schedule.dao;
+
+import com.galaxyinternet.export_schedule.model.ScheduleInfo;
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.GrantPart;
+
+public interface ScheduleInfoDao extends BaseDao<ScheduleInfo, Long>{
+
+}
---
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/export_schedule/service/ScheduleInfoService.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.export_schedule.service;
+
+import com.galaxyinternet.export_schedule.model.ScheduleInfo;
+import com.galaxyinternet.framework.core.service.BaseService;
+
+public interface ScheduleInfoService extends BaseService<ScheduleInfo>{
+
+}
