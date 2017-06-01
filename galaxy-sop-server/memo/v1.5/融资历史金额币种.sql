bc305d6fc7e706638c07fbd535f857e2fef3ba6a
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-06-01 14:25:00 +0800

    user manage part

--- galaxy-sop-server/memo/v1.5/融资历史金额币种.sql
+++ galaxy-sop-server/memo/v1.5/融资历史金额币种.sql
@@ -3,3 +3,6 @@ INSERT INTO `fx_db`.`dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_cod
 INSERT INTO `fx_db`.`dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `created_time`, `updated_time`, `is_delete`) VALUES ('208', 'currency', '美元', '1', 'currency:1', '2', '1456382861380', '1456382861380', '0');
 INSERT INTO `fx_db`.`dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `created_time`, `updated_time`, `is_delete`) VALUES ('209', 'currency', '英镑', '2', 'currency:2', '3', '1456382861380', '1456382861380', '0');
 INSERT INTO `fx_db`.`dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `created_time`, `updated_time`, `is_delete`) VALUES ('211', 'currency', '欧元', '3', 'currency:3', '4', '1456382861380', '1456382861380', '0');
+
+
+
