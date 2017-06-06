6bedd85ff74a4471a39b5f469ad9a4bf1982fc95
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-06-06 17:51:49 +0800

    update

--- galaxy-sop-server/memo/v1.5/addcolumn.sql
+++ galaxy-sop-server/memo/v1.5/addcolumn.sql
@@ -18,14 +18,14 @@ insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dic
 insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('215','financeMode','领投','2','financeMode:2','2',NULL,'145638261380','145638261380','0');
 
 CREATE TABLE `joint_delivery` (
-  `id` BIGINT(100) NOT NULL AUTO_INCREMENT,
-  `delivery_type` TINYINT(1) DEFAULT NULL COMMENT '0-独投;1-领投;2-合投',
-  `delivery_name` VARCHAR(20) DEFAULT NULL COMMENT '合投机构名称',
-  `delivery_amount` DECIMAL(15,4) DEFAULT NULL COMMENT '投资金额',
-  `delivery_share_ratio` DECIMAL(6,4) DEFAULT NULL COMMENT '股权占比',
-  `project_id` BIGINT(50) DEFAULT NULL COMMENT '项目ID',
-  `create_uid` BIGINT(50) DEFAULT NULL COMMENT '创建人',
-  `create_time` DATE DEFAULT NULL COMMENT '创建日期',
-  `update_time` DATE DEFAULT NULL COMMENT '修改日期',
+  `id` bigint(100) NOT NULL AUTO_INCREMENT,
+  `delivery_type` tinyint(1) DEFAULT NULL COMMENT '0-独投;1-领投;2-合投',
+  `delivery_name` varchar(20) DEFAULT NULL COMMENT '合投机构名称',
+  `delivery_amount` decimal(15,4) DEFAULT NULL COMMENT '投资金额',
+  `delivery_share_ratio` decimal(6,4) DEFAULT NULL COMMENT '股权占比',
+  `project_id` bigint(50) DEFAULT NULL COMMENT '项目ID',
+  `create_uid` bigint(50) DEFAULT NULL COMMENT '创建人',
+  `create_time` bigint(30) DEFAULT NULL COMMENT '创建日期',
+  `update_time` bigint(30) DEFAULT NULL COMMENT '修改日期',
   PRIMARY KEY (`id`)
-  ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
