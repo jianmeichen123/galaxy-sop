418c70981e78b0969a34996b6ad2eaed9e8fe9bd
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-06-08 11:28:12 +0800

    update

--- galaxy-sop-server/src/main/java/com/galaxyinternet/project/service/JointDeliveryServiceImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/project/service/JointDeliveryServiceImpl.java
@@ -1,19 +1,24 @@
 package com.galaxyinternet.project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.galaxyinternet.dao.project.JointDeliveryDao;
 import com.galaxyinternet.framework.core.dao.BaseDao;
 import com.galaxyinternet.framework.core.service.impl.BaseServiceImpl;
 import com.galaxyinternet.model.project.JointDelivery;
 import com.galaxyinternet.service.JointDeliveryService;
 
-@Service("jointDeliveryService")
+@Service("com.galaxyinternet.service.jointDeliveryService")
 public class JointDeliveryServiceImpl extends BaseServiceImpl<JointDelivery> implements JointDeliveryService{
 
+	@Autowired
+	private JointDeliveryDao jointDeliveryDao;
+	
 	@Override
 	protected BaseDao<JointDelivery, Long> getBaseDao() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.jointDeliveryDao;
 	}
 
 }
