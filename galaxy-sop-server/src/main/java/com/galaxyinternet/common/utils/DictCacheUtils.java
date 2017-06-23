f3301831a7c395c4e157af6cd973698172c5d628
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-06-23 16:50:28 +0800

    update

--- galaxy-sop-server/src/main/java/com/galaxyinternet/common/utils/DictCacheUtils.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/common/utils/DictCacheUtils.java
@@ -1,7 +1,9 @@
 package com.galaxyinternet.common.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,6 @@ import org.springframework.stereotype.Component;
 import com.galaxyinternet.framework.cache.Cache;
 import com.galaxyinternet.model.dict.Dict;
 import com.galaxyinternet.service.DictService;
-import com.galaxyinternet.utils.SopConstatnts;
 
 
 @Component(value="dictCacheUtils")
@@ -19,7 +20,7 @@ public class DictCacheUtils{
 	private  DictService dictService;
 	
 	@Autowired
-	private  Cache cache;
+	private Cache cache;
 	
 	/**
 		设置过时时间， 重新检索时间，单位秒，2小时 = 4 * 60 * 60
@@ -35,23 +36,27 @@ public class DictCacheUtils{
 	 */
 	public void initIds(){
 		List<Dict> queryAll = dictService.queryAll();
-		List<Dict> parentDictList=new ArrayList<Dict>();
+		Map<String,Dict> map=new HashMap<String,Dict>();
 		if(null!=queryAll&&!queryAll.isEmpty()){
 			for(Dict dict:queryAll){
+				List<Dict> parentDictList=new ArrayList<Dict>();
 				//判断如果parentCode=xhhl，父级接点（根据父级dictCode得到父级下面的子集list）
 				if(dict.getParentCode().equals("xhhl")){
-					parentDictList.add(dict);
-					saveParentRedis(dict.getCode(), parentDictList);
+					saveParentRedis(dict.getParentCode()+"_"+dict.getCode(), parentDictList);
 				}else{
 				   //子集保存到相应的父级下面
-					List<Dict> list = getCachValues(dict.getParentCode().toString());
+					List<Dict> list = getCachValues("xhhl"+"_"+dict.getParentCode());
 					if(null==list||list.isEmpty()){
 						list=new ArrayList<Dict>();
 					}
-					saveParentRedis(dict.getParentCode(),list);
+					list.add(dict);
+					saveParentRedis("xhhl"+"_"+dict.getParentCode(),list);
+					map.put(dict.getCode()+"_"+dict.getId(),dict);
 				}
-				saveDictRedis(dict.getCode(),dict);
+		
+				
 			}
+			saveDictRedis("xhhl_dict",map);
 		}
 	}
 	
@@ -70,9 +75,9 @@ public class DictCacheUtils{
 		ids值 保存到 redis， 
 		初始化 redis值时 用到
 	*/
-	public synchronized void saveDictRedis(String key,Dict dict){
+	public synchronized void saveDictRedis(String key,Map<String,Dict> dict){
 		if(dict == null){
-			dict =new Dict();
+			dict =new HashMap<String,Dict>();
 		}
 		cache.set(key, dict);
 	}
@@ -81,9 +86,9 @@ public class DictCacheUtils{
 		ids值 保存到 redis， 
 		初始化 redis值时 用到
 	*/
-	public synchronized void saveDictNameRedis(String key,String dictName){
+	public synchronized void saveDictNameRedis(String key,Map<String,String> dictName){
 		if(dictName == null){
-			dictName ="";
+			dictName =new HashMap<String,String>();
 		}
 		cache.set(key, dictName);
 	}
@@ -114,36 +119,66 @@ public class DictCacheUtils{
 		}
 	}
 	
-	/*@SuppressWarnings("unchecked")
+     /*@SuppressWarnings("unchecked")
 	public static List<Dict> getParentDictList(String key){
 		List<Dict> values = new ArrayList<Dict>();
-		//Object obj = cache.get(key);
+		Object obj = cache.get(key);
 		if(obj != null){
 			values = (List<Dict>) obj;
 		}
 		return values;
 	}
 	
-	
+	*//**
+	 * 根据数据字典code+id 作为key获取某个数据字典类
+	 * @param key
+	 * @return
+	 *//*
+	@SuppressWarnings("unchecked")
 	public static Dict getDict(String key){
 		Dict values = new Dict();
-	//	Object obj = cache.get(key);
+		Map<String,Dict> map=new HashMap<String,Dict>();
+		Object obj = cache.get("xhhl_dict");
 		if(obj != null){
-			values = (Dict) obj;
+			map = (Map<String,Dict>) obj;
+		}
+		values=map.get(key);
+		if(null==values){
+			values=new Dict();
 		}
 		return values;
 	}
-	
+	*//**
+	 * 根据数据字典code+id 作为key获取某个数据字典类
+	 * @param key
+	 * @return
+	 *//*
    public static String getDictName(String key){
-		String values = "";
-		Object obj = cache.get(key);
+	     Dict values = new Dict();
+	     String dictName="";
+		Map<String,Dict> map=new HashMap<String,Dict>();
+		Object obj = cache.get("xhhl_dict");
 		if(obj != null){
-			values = (String) obj;
+			map = (Map<String,Dict>) obj;
 		}
-		return values;
+		values=map.get(key);
+		if(null==values){
+			dictName="";
+		}
+		return dictName;
 	}*/
 	public static void main(String[] args){
 		DictCacheUtils o=new DictCacheUtils();
 		o.initIds();
+	}
+	@Autowired
+	public void execute()  {
+			try {
+				executeInteral();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 	}
 }
