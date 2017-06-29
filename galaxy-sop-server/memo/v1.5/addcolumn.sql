0e867d827f70bac60d8d263fdabafc05c1b8db52
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-06-29 17:02:13 +0800

    update

--- galaxy-sop-server/memo/v1.5/addcolumn.sql
+++ galaxy-sop-server/memo/v1.5/addcolumn.sql
@@ -14,12 +14,12 @@ ADD COLUMN `finance_mode` VARCHAR(45) NULL COMMENT '投资形式' AFTER `project
 
 insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('212','xhhl','投资形式','30','financeMode','30','投资形式（项目基本信息）','1456382861380','1456382861380','0');
 insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('213','financeMode','独投','0','financeMode:0','0',NULL,'145638261380','145638261380','0');
-insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('214','financeMode','合投','1','financeMode:1','1',NULL,'145638261380','145638261380','0');
-insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('215','financeMode','领投','2','financeMode:2','2',NULL,'145638261380','145638261380','0');
+insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('214','financeMode','领投','1','financeMode:1','1',NULL,'145638261380','145638261380','0');
+insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('215','financeMode','合投','2','financeMode:2','2',NULL,'145638261380','145638261380','0');
 
 CREATE TABLE `joint_delivery` (
   `id` bigint(100) NOT NULL AUTO_INCREMENT,
-  `delivery_type` tinyint(1) DEFAULT NULL COMMENT '0-独投;1-领投;2-合投',
+  `delivery_type` varchar(20) DEFAULT NULL COMMENT '0-独投;1-领投;2-合投',
   `delivery_name` varchar(20) DEFAULT NULL COMMENT '合投机构名称',
   `delivery_amount` decimal(15,4) DEFAULT NULL COMMENT '投资金额',
   `delivery_share_ratio` decimal(6,4) DEFAULT NULL COMMENT '股权占比',
