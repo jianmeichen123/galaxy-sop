f6df54d205876379e535ec16d7acd5056269129a
liuyaxin <yaxinliu@galaxyinternet.com>
2017-07-20 10:34:03 +0800

    tag:添加文件表中关联会议和关联访谈的ID

--- galaxy-sop-server/memo/v1.5/sop_file.sql
+++ galaxy-sop-server/memo/v1.5/sop_file.sql
@@ -1,3 +1,7 @@
 
-	ALTER TABLE `fx_db`.`sop_file`   
+ALTER TABLE `fx_db`.`sop_file`   
 	  CHANGE `fil_uri` `fil_uri` VARCHAR(300) CHARSET utf8 COLLATE utf8_bin NULL  COMMENT '存储地址';
+	  
+	  
+ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `interview_record_id` bigint(20) NULL COMMENT '关联访谈ID';
+ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `meeting_record_id` bigint(20) NULL COMMENT '关联会议ID';
