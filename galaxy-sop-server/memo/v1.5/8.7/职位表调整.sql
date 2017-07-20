1d8912cb5e688d698682bf3ad13dc71c2a6721fe
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-07-20 18:13:02 +0800

    update

---
+++ galaxy-sop-server/memo/v1.5/8.7/职位表调整.sql
@@ -0,0 +1,4 @@
+UPDATE `power`.`position` SET `is_del`='0', `is_outtage`='0', `company_id`='1' WHERE `id`='24';
+UPDATE `power`.`position` SET `is_del`='0', `is_outtage`='0', `company_id`='1' WHERE `id`='25';
+UPDATE `power`.`position` SET `is_del`='0', `is_outtage`='0', `company_id`='1' WHERE `id`='26';
+UPDATE `power`.`position` SET `is_del`='0', `is_outtage`='0', `company_id`='1' WHERE `id`='27';
