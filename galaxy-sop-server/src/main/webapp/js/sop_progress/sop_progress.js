bf3ed558d85fb2903fbf7c6fca50e6f4e147e167
liuyaxin <yaxinliu@galaxyinternet.com>
2017-08-02 15:03:37 +0800

    tag:字符长度校验

--- galaxy-sop-server/src/main/webapp/js/sop_progress/sop_progress.js
+++ galaxy-sop-server/src/main/webapp/js/sop_progress/sop_progress.js
@@ -450,8 +450,8 @@ function getInterViewParams(hasProid,projectId,
 		layer.msg("项目不能为空");
 		return false;
 	}
-	if(viewNotesId != null && viewNotesId.length > 0){
-		if(delHtmlTag(viewNotesId).length > 5000){
+	if(viewNotes != null && viewNotes.length > 0){
+		if(delHtmlTag(viewNotes).length > 5000){
 			layer.msg("访谈记录长度最大5000字");
 			return false;
 		}
