4c4450af740e707233802388f4d8b128cceab0ab
liuyaxin <yaxinliu@galaxyinternet.com>
2017-08-02 16:10:48 +0800

    tag:bug

--- galaxy-sop-server/src/main/webapp/js/sop_progress/sop_file.js
+++ galaxy-sop-server/src/main/webapp/js/sop_progress/sop_file.js
@@ -307,12 +307,13 @@ function getOptionStr(file,type){
 	var other_class="";
 	if(file.fileSuffix!='JPG'&&file.fileSuffix!='jpg'&&file.fileSuffix!='jpeg'&&file.fileSuffix!='png'&&file.fileSuffix!='PNG' && file.fileSuffix!='xls'&&file.fileSuffix!='xlsx'&&file.fileSuffix!='XLS'&&file.fileSuffix!='XLSX'){
 		var other_class="downlond_other";
+		type = "other";
 	}
 	if(file.canOpt){
 		optOption += '<span type="reupload" class="reupload_'+type+'" id="'+ file.fileWorktype.replace(":","_") +'_up"  data-type="' + file.fileWorktype + '"></span> ';
 	}
 	if(file.canDown){
-		optOption += '<span type="downlond" class="'+other_class+' downlond_'+type+'" id="'+ file.fileWorktype.replace(":","_") +'_down" onclick="filedown(\''+file.id+'\')"></span>';
+		optOption += '<span type="downlond" class=" downlond_'+type+'" id="'+ file.fileWorktype.replace(":","_") +'_down" onclick="filedown(\''+file.id+'\')"></span>';
 	}
 	if(optOption != '' && optOption.length > 0){
 		//var filetype = file.fileSuffix;
