171d00d5708c564b677b66779d43cd68737d7f43
liuyaxin <yaxinliu@galaxyinternet.com>
2017-08-02 15:13:12 +0800

    tag:提交

--- galaxy-sop-server/memo/v1.5/8.7/sop.sql
+++ galaxy-sop-server/memo/v1.5/8.7/sop.sql
@@ -1,37 +1,3 @@
-/*历史数据流程历史处理*/
-UPDATE sop_project SET progress_history=
-CASE project_progress 
-WHEN 'projectProgress:1' THEN 'projectProgress:1'
-WHEN 'projectProgress:2' THEN 'projectProgress:1,projectProgress:2'
- WHEN 'projectProgress:3' THEN 'projectProgress:1,projectProgress:2,projectProgress:3'
- WHEN 'projectProgress:4' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4'
- WHEN 'projectProgress:11' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11'
- WHEN 'projectProgress:5' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5'
-WHEN 'projectProgress:6' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6'
- WHEN 'projectProgress:7' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7'
-WHEN 'projectProgress:8' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8'
- WHEN 'projectProgress:9' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8,projectProgress:9'
-WHEN 'projectProgress:10' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8,projectProgress:9,projectProgress:10'
-ELSE '' END ;
-	ALTER TABLE `fx_db`.`sop_file`   
-	  CHANGE `fil_uri` `fil_uri` VARCHAR(300) CHARSET utf8 COLLATE utf8_bin NULL  COMMENT '存储地址';
-
-ALTER TABLE `fx_db`.`sop_project`   
-  ADD COLUMN `progress_history` VARCHAR(255) NULL  COMMENT '流程历史记录(逗号分割)';
-ALTER TABLE `fx_db`.`sop_project`  
-  ADD COLUMN `business_type_code` VARCHAR(20) NULL  COMMENT '业务类型编码：TZ-投资，ST-闪投'; 
-  
-   ALTER TABLE `fx_db`.`sop_interview_record` 
-ADD COLUMN `interview_result` VARCHAR(32) NULL COMMENT '访谈结果' AFTER `schedule_id`;
-  ALTER TABLE `fx_db`.`sop_interview_record` 
-ADD COLUMN `result_reason` VARCHAR(32) NULL COMMENT '原因' AFTER `interview_result`;
-  ALTER TABLE `fx_db`.`sop_interview_record` 
-ADD COLUMN `reason_other` VARCHAR(32) NULL COMMENT '其他原因' AFTER `result_reason`;
-  ALTER TABLE `fx_db`.`sop_meeting_record` 
-ADD COLUMN `result_reason` VARCHAR(32) NULL COMMENT '原因' AFTER `update_time`;
-  ALTER TABLE `fx_db`.`sop_meeting_record` 
-ADD COLUMN `reason_other` VARCHAR(32) NULL COMMENT '其他原因' AFTER `result_reason`;
-
 INSERT INTO `dict` (`id`,`parent_code`,`name`,`dict_value`,`dict_code`,`dict_sort`,`text`,`created_time`,`updated_time`,`is_delete`) VALUES (181,'meeting3Result','补充材料',1,'meeting3Result:1',1,'立项会结果-补充材料','1456382861380','1456382861380',0);
 INSERT INTO `dict` (`id`,`parent_code`,`name`,`dict_value`,`dict_code`,`dict_sort`,`text`,`created_time`,`updated_time`,`is_delete`) VALUES (182,'meeting3Result','闪投',2,'meeting3Result:2',2,'立项会结果-闪投','1456382861380','1456382861380',0);
 INSERT INTO `dict` (`id`,`parent_code`,`name`,`dict_value`,`dict_code`,`dict_sort`,`text`,`created_time`,`updated_time`,`is_delete`) VALUES (183,'meeting3Result','投资',3,'meeting3Result:3',3,'立项会结果-投资','1456382861380','1456382861380',0);
@@ -73,25 +39,35 @@ UPDATE `fx_db`.`dict` SET `dict_sort`='11' WHERE `id`='37';
 
 UPDATE `fx_db`.`dict` SET `name`='投资估值未达成一致' WHERE `id`='196';
 UPDATE `fx_db`.`dict` SET `name`='投资方式未达成一致（如并购、财务投资，服务费等）' WHERE `id`='197';
+
 /**
  * 放弃的任务对其任务对应的文件进行处理
  */
 insert into `dict` (`id`, `parent_code`, `name`, `dict_value`, `dict_code`, `dict_sort`, `text`, `created_time`, `updated_time`, `is_delete`) values('216','fileStatus','已放弃','4','fileStatus:4','4','文档状态','145638261380','145638261380','0');
+
+
+ALTER TABLE `fx_db`.`sop_file`   
+	  CHANGE `fil_uri` `fil_uri` VARCHAR(300) CHARSET utf8 COLLATE utf8_bin NULL  COMMENT '存储地址';
+
+ALTER TABLE `fx_db`.`sop_project`   
+  ADD COLUMN `progress_history` VARCHAR(255) NULL  COMMENT '流程历史记录(逗号分割)';
+ALTER TABLE `fx_db`.`sop_project`  
+  ADD COLUMN `business_type_code` VARCHAR(20) NULL  COMMENT '业务类型编码：TZ-投资，ST-闪投'; 
+  
+   ALTER TABLE `fx_db`.`sop_interview_record` 
+ADD COLUMN `interview_result` VARCHAR(32) NULL COMMENT '访谈结果' AFTER `schedule_id`;
+  ALTER TABLE `fx_db`.`sop_interview_record` 
+ADD COLUMN `result_reason` VARCHAR(32) NULL COMMENT '原因' AFTER `interview_result`;
+  ALTER TABLE `fx_db`.`sop_interview_record` 
+ADD COLUMN `reason_other` VARCHAR(32) NULL COMMENT '其他原因' AFTER `result_reason`;
+  ALTER TABLE `fx_db`.`sop_meeting_record` 
+ADD COLUMN `result_reason` VARCHAR(32) NULL COMMENT '原因' AFTER `update_time`;
+  ALTER TABLE `fx_db`.`sop_meeting_record` 
+ADD COLUMN `reason_other` VARCHAR(32) NULL COMMENT '其他原因' AFTER `result_reason`;
 ALTER TABLE `fx_db`.`sop_interview_record` 
 ADD COLUMN `notes_remark` TEXT NULL COMMENT '访谈纪要备份字段' AFTER `reason_other`;
 ALTER TABLE `fx_db`.`sop_meeting_record` 
 ADD COLUMN `notes_remark` TEXT NULL COMMENT '会议纪要备份字段' AFTER `reason_other`;
-
-UPDATE sop_meeting_record SET notes_remark=meeting_notes;
-UPDATE sop_interview_record SET notes_remark=view_notes;
- UPDATE power.resource SET resource_type = 5 
-WHERE resource_code IN (
-'dataBriefing','project_view_module','div_duration_gg',
-'div_performance_gg','div_pro_doc','div_idea',
-'div_matter_preview_gg','div_project_progress','div_health_gg',
-'task_list','div_project_post_analysis_gg');
-ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `interview_record_id` bigint(20) NULL COMMENT '关联访谈ID';
-ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `meeting_record_id` bigint(20) NULL COMMENT '关联会议ID';
 
 /**
  * 更改脚本
@@ -107,4 +83,31 @@ alter table `fx_db`.`sop_interview_record` modify column reason_other varchar(50
 alter table `fx_db`.`sop_meeting_record` modify column reason_other varchar(50) COMMENT '其他原因';
 UPDATE dict SET NAME = '人事尽职调查报告' WHERE NAME= '人力资源尽职调查报告';
 UPDATE sop_project  SET business_type_code='TZ' WHERE business_type_code IS NULL;
+
+UPDATE sop_meeting_record SET notes_remark=meeting_notes;
+UPDATE sop_interview_record SET notes_remark=view_notes;
+ UPDATE power.resource SET resource_type = 5 
+WHERE resource_code IN (
+'dataBriefing','project_view_module','div_duration_gg',
+'div_performance_gg','div_pro_doc','div_idea',
+'div_matter_preview_gg','div_project_progress','div_health_gg',
+'task_list','div_project_post_analysis_gg');
+ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `interview_record_id` bigint(20) NULL COMMENT '关联访谈ID';
+ALTER TABLE `fx_db`.`sop_file` ADD COLUMN `meeting_record_id` bigint(20) NULL COMMENT '关联会议ID';
+
+/*历史数据流程历史处理*/
+UPDATE sop_project SET progress_history=
+CASE project_progress 
+WHEN 'projectProgress:1' THEN 'projectProgress:1'
+WHEN 'projectProgress:2' THEN 'projectProgress:1,projectProgress:2'
+ WHEN 'projectProgress:3' THEN 'projectProgress:1,projectProgress:2,projectProgress:3'
+ WHEN 'projectProgress:4' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4'
+ WHEN 'projectProgress:11' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11'
+ WHEN 'projectProgress:5' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5'
+WHEN 'projectProgress:6' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6'
+ WHEN 'projectProgress:7' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7'
+WHEN 'projectProgress:8' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8'
+ WHEN 'projectProgress:9' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8,projectProgress:9'
+WHEN 'projectProgress:10' THEN 'projectProgress:1,projectProgress:2,projectProgress:3,projectProgress:4,projectProgress:11,projectProgress:5,projectProgress:6,projectProgress:7,projectProgress:8,projectProgress:9,projectProgress:10'
+ELSE '' END ;
 
