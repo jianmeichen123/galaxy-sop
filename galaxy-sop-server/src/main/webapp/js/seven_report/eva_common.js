4541239ed77dcfb8b06cf644aff27d63518bc1ea
liulijun <11141736405@qq.com>
2017-08-14 14:57:53 +0800

    正在编辑时无法保存

--- galaxy-sop-server/src/main/webapp/js/seven_report/eva_common.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/eva_common.js
@@ -595,6 +595,20 @@ function getTalbleData()
 	
 }
 $("#save-rpt-btn").click(function(){
+	//判断是否让其进行保存
+	var editbox = $(".radioShow");
+	var edit_status = false;
+	$.each(editbox,function(){
+		console.log(!$(this).is(":hidden"))
+		if(!$(this).is(":hidden")){
+			edit_status = true;
+			return false;
+		}
+		return edit_status; 
+	})
+	if(edit_status==true){
+		alert("正在编辑无法保存");
+	}else{
 	var data = {
 		projectId: projId,
 		scoreList:	getScores(),
@@ -614,6 +628,8 @@ $("#save-rpt-btn").click(function(){
 					layer.msg(data.result.message);
 				}
 		})
+		
+}
 });
 
 //type_8  展开收起公共方法
