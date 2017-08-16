a379be614f3b2bb990d85d60d04ed29dcfe8e043
chen <3443999822@qq.com>
2017-08-16 10:08:06 +0800

    修改弹窗位置；

--- galaxy-sop-server/src/main/webapp/js/seven_report/eva_report/seven_basic.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/eva_report/seven_basic.js
@@ -53,7 +53,7 @@
 			$('.gapPopup').show();
 			$(obj).attr("parent_dom","show");
 			var  leftNum = _this.offset().left-34;
-			var  topNum = _this.offset().top-$(".gapPopup").height()-174;
+			var  topNum = _this.offset().top-$(".gapPopup").height()-165;
 			$('.gapPopup').css('left',leftNum).css('top',topNum);
 			$('.mashLayer').show();
 			//请求成功，数据渲染模板edit_tmpl1
