4d2340d7dd22b99e2f39ff0dc5f0d56250ceb448
zhangfeng <fengzhang@falaxyinternet.com>
2017-08-16 15:00:14 +0800

    title completed progress

---
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/dao/InformationProgressDaoImpl.java
@@ -0,0 +1,14 @@
+package com.galaxyinternet.hologram.dao;
+
+import com.galaxyinternet.dao.hologram.InformationProgressDao;
+import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.model.hologram.InformationProgress;
+import org.springframework.stereotype.Repository;
+
+@Repository("informationProgressDao")
+public class InformationProgressDaoImpl extends BaseDaoImpl<InformationProgress, Long> implements InformationProgressDao {
+
+
+
+	
+}
