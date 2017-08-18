614937559d0a5282e36c7872826288df5f12a7b3
liulijun <11141736405@qq.com>
2017-08-18 16:23:28 +0800

    c

--- galaxy-sop-server/src/main/webapp/js/seven_report/eva_report/seven_basic.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/eva_report/seven_basic.js
@@ -767,7 +767,6 @@ $('div').delegate(".h_save_btn","click",function(event){
 						_this.find("span").html("未添加");
 					}else{
 						$.each(d_this.valueList,function(i,n){
-							console.log(n.id)
 							if(n.id==undefined){
 								var a_img="<em class=\"income_pic\" data-url="+n.url+" data-list="+i+">[图片]</em>";
 								
