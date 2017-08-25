909dc7bc5ebd77be866dbf857ca8c8fc9fb9599f
zhangfeng <fengzhang@falaxyinternet.com>
2017-08-25 18:09:22 +0800

    title completed progress

--- galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationDictionaryServiceImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationDictionaryServiceImpl.java
@@ -480,7 +480,11 @@ public class InformationDictionaryServiceImpl extends BaseServiceImpl<Informatio
 				String newStr=fieldList.get(i).substring(0, 1).toUpperCase()+fieldList.get(i).replaceFirst("\\w","");
 				String methodStr="get"+newStr;
 				Method getMethod = InformationListdata.class.getMethod(methodStr);
-				String fileValue = (String) getMethod.invoke(temp);
+				Object obj = getMethod.invoke(temp);
+                String fileValue = "";
+                if(obj != null){
+                    fileValue = (String) obj ;
+                }
 
 				if(fieldList.get(i).equals("field2") || fieldList.get(i).equals("field4")){ //胜算
 					if(StringUtils.isBlank(fileValue)){
