2b902b9b7f9706009775afdb2d00ad4aa85a0ea5
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-09-06 16:08:18 +0800

    111

--- galaxy-sop-server/memo/v1.7/8列表选择题调整为字典项Value.sql
+++ galaxy-sop-server/memo/v1.7/8列表选择题调整为字典项Value.sql
@@ -13,8 +13,11 @@ set a.field_2 = t2.id where a.title_id = 1303 and a.code = 'team-members';
 
 
 /*综合竞争比较 措施的有效性是否验证字段调整为字典项*/
-update fx_db.information_listdata a inner join fx_db.information_dictionary t2 on  t2.name = a.field_5  and t2.parent_id = 1551
-set a.field_5 = t2.id where a.title_id = 1548 ;
+UPDATE fx_db.information_listdata a INNER JOIN fx_db1.information_dictionary t2 ON  t2.name 
+ = a.field_5  AND t2.parent_id = 1941
+SET a.field_5 = t2.id 
+ WHERE a.title_id = 1548 ;
+
 
 /*股权结构 股东类型字段调整为字典项*/
 update fx_db.information_listdata a inner join fx_db.information_dictionary t2 on  t2.name = a.field_3  and t2.parent_id = 2091
