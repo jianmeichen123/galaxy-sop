daef34ee16893e6c2efc74eee11153073a48de65
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-09-14 14:43:27 +0800

    111111111111

---
+++ galaxy-sop-server/memo/v1.8/update.sql
@@ -0,0 +1,4 @@
+--表格增加查看功能（综合竞争比较，上一轮融资后关键运营指标变化，每次融资的里程碑和时间节点如何）
+UPDATE `fx_db`.`information_listdata_remark` SET `fun_flag`='1' WHERE `id`='6';
+UPDATE `fx_db`.`information_listdata_remark` SET `fun_flag`='1' WHERE `id`='9';
+UPDATE `fx_db`.`information_listdata_remark` SET `fun_flag`='1' WHERE `id`='11';
