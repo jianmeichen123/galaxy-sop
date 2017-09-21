e0ff3d14e904cd8e1f79eeabbe6912cdfbd5a8d8
chen <3443999822@qq.com>
2017-09-21 13:46:45 +0800

    增加查看

--- galaxy-sop-server/src/main/webapp/js/seven_report/basic_fun.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/basic_fun.js
@@ -495,16 +495,19 @@ function editRow(ele)
 			 if(txt=="查看"){
 					$("#detail-form").hide();
 					$(".button_affrim").hide();
+					$('#detail-form_look_over').show();
 					$("#delivery_popup_name").text("查看交割事项");
 					 $('#grant_popup_name').html('查看分期注资计划');
 					 $('#finace_popup_name').html('查看融资历史');
 					 $("#complete_title").html('查看综合竞争比较');
 					 $("#pop-title-gs").text('查看同类公司');
-					 $("#pop-title-time").text('查看里程碑和时间节点');
+					 $("#pop-title-time").text('查看融资的里程碑和时间节点');
 					 $("#pop-title").text('查看分期注资计划');
+					 $("#pop-title-yy").html('查看关键运营指标变化');
 					  $(".appr_part_add").hide();   //添加实际注资计划按钮
 				}else{
 					$(".see_block").hide();
+					$('#detail-form_look_over').hide();
 					$("#delivery_popup_name").text("编辑交割事项");
 					 $('#grant_popup_name').html('编辑分期注资计划');
 					 $('#finace_popup_name').html('编辑融资历史');
