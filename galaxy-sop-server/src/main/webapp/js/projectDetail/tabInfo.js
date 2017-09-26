36e48be41d1b0c629cf1c4df0ec7d2237756e90b
liulijun <11141736405@qq.com>
2017-09-26 17:17:10 +0800

    clean

--- galaxy-sop-server/src/main/webapp/js/projectDetail/tabInfo.js
+++ galaxy-sop-server/src/main/webapp/js/projectDetail/tabInfo.js
@@ -114,16 +114,16 @@ $(function(){
 			$("#createUname").text(projectInfo.createUname);
 			$("#projectCareerline").text(projectInfo.projectCareerline);
 			$("#projectType").text(projectInfo.type);
-			/*$("#project_contribution").text(typeof(projectInfo.projectContribution)=="undefined"?"--":(projectInfo.projectContribution==0?"--":projectInfo.projectContribution*10000/10000));
-			$("#project_valuations").text(typeof(projectInfo.projectValuations)=="undefined"?"--":(projectInfo.projectValuations==0?"--":projectInfo.projectValuations*10000/10000));*/
+			$("#project_contribution").text(typeof(projectInfo.projectContribution)=="undefined"?"--":(projectInfo.projectContribution==0?"--":projectInfo.projectContribution*10000/10000));
+			$("#project_valuations").text(typeof(projectInfo.projectValuations)=="undefined"?"--":(projectInfo.projectValuations==0?"--":projectInfo.projectValuations*10000/10000));
 			$("#project_share_ratio").text(typeof(projectInfo.projectShareRatio)=="undefined"?"--":(projectInfo.projectShareRatio==0?"--":projectInfo.projectShareRatio*10000/10000));
 			$("#projectProgress").text(projectInfo.progress);
 			$("#projectStatusDs").text(projectInfo.projectProgress=="projectProgress:10"?"":projectInfo.projectStatusDs);
-			$("#financeStatusDs").text(projectInfo.financeStatusDs==null?"不明确":projectInfo.financeStatusDs);/*
+			$("#financeStatusDs").text(projectInfo.financeStatusDs==null?"不明确":projectInfo.financeStatusDs);
 			$("#finalValuations").text(typeof(projectInfo.finalValuations)=="undefined"?"--":(projectInfo.finalValuations==0?"--":projectInfo.finalValuations*10000/10000));
 			$("#finalContribution").text(typeof(projectInfo.finalContribution)=="undefined"?"--":(projectInfo.finalContribution==0?"--":projectInfo.finalContribution*10000/10000));
 			$("#finalShareRatio").text(typeof(projectInfo.finalShareRatio)=="undefined"?"--":(projectInfo.finalShareRatio==0?"--":projectInfo.finalShareRatio*10000/10000));
-			$("#serviceCharge").text(typeof(projectInfo.serviceCharge)=="undefined"?"--":(projectInfo.serviceCharge==0?"--":projectInfo.serviceCharge*10000/10000));*/
+			$("#serviceCharge").text(typeof(projectInfo.serviceCharge)=="undefined"?"--":(projectInfo.serviceCharge==0?"--":projectInfo.serviceCharge*10000/10000));
 			$("#industryOwnDs").text(projectInfo.industryOwnDs);
 			$("#faName").text(projectInfo.faFlag=="projectSource:1"?"FA-"+projectInfo.faName:map_projectSource[projectInfo.faFlag]);
 		    $("#remarkStr").text(projectInfo.remark==""?"无":(projectInfo.remark==null?"无":projectInfo.remark));
@@ -901,7 +901,7 @@ function buildMoneyResult(pid){
 							var title = this;
 							$("input[data-title-id='"+title.id+"']").attr("data-type",title.type);	
 							if(null!=title.resultList&&title.resultList.length>0){
-								var _val = title.resultList[0].contentDescribe1
+								var _val = title.resultList[0].contentDescribe1;
 								$(".new_color_black[data-title-id='"+title.id+"']").text(_val==undefined ?"--":_val*100000/100000);
 								$("input[data-title-id='"+title.id+"']").val(_val==undefined ?"":_val*100000/100000).attr({"data-result-id":title.resultList[0].id});	
 							}
