9155f268a65558538ce30c616897817e2ebedd35
liuyaxin <yaxinliu@galaxyinternet.com>
2017-09-27 17:21:09 +0800

    --------

--- galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/service/ReportExportServiceImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/hologram/service/ReportExportServiceImpl.java
@@ -689,6 +689,7 @@ public class ReportExportServiceImpl implements ReportExportService {
         params.put("titleIds",ids);
         params.put("projectId",projectId);
         params.put("notAllNUll",true);
+        params.put("property","result.created_time ASC");
         List<InformationTitle> titleList = informationTitleDao.selectTitleOfFileResults(params);
 
         if(titleList == null || titleList.isEmpty()){
