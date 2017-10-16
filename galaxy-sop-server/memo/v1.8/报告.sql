84fbfd5baf745da924e5734c480739e8e850fdbf
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-10-16 17:55:42 +0800

    111

--- galaxy-sop-server/memo/v1.8/报告.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `fx_db`.`information_dictionary` SET `name`='新三板' WHERE `id`='2198';
-
-UPDATE `fx_db`.`dict` SET `is_delete`='1' WHERE `id`='209';
-UPDATE `fx_db`.`dict` SET `is_delete`='1' WHERE `id`='211';
