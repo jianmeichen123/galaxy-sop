4e008347675e6e41980996eceec3a91fa3197da9
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-10-16 18:45:13 +0800

    111111

---
+++ galaxy-sop-server/memo/v1.8/cunchu.sql
@@ -0,0 +1,6 @@
+CALL `historical_data_grant`();
+
+CALL `historical_data_shares`();
+CALL `historical_data_finance_history`();
+CALL `historical_data_project_team`();
+CALL `historical_data_delivery`();
