438dbb4052761319e71958cd5af1ec9bb98cf9c2
liuyaxin <yaxinliu@galaxyinternet.com>
2017-10-30 17:29:51 +0800

    tag:获取项目信息列表

--- galaxy-sop-api/src/main/java/com/galaxyinternet/dao/hologram/InformationResultDao.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/hologram/InformationResultDao.java
@@ -1,8 +1,9 @@
 package com.galaxyinternet.dao.hologram;
 
+import java.util.List;
 import com.galaxyinternet.framework.core.dao.BaseDao;
 import com.galaxyinternet.model.hologram.InformationResult;
 
 public interface InformationResultDao extends BaseDao<InformationResult, Long>{
-
+	List<InformationResult> selectResultByRelate(InformationResult ir);
 }
