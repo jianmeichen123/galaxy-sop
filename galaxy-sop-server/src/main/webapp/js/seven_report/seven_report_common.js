bbdbe1e3386e3cc0c66b70c125d2bff19f958c59
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-11-27 18:56:18 +0800

    update

--- galaxy-sop-server/src/main/webapp/js/seven_report/seven_report_common.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/seven_report_common.js
@@ -467,9 +467,13 @@ function buildResults(sec,title,readonly)
 				}
 				dds.html((title.resultList[0].contentDescribe1==undefined || sum==0) ?"未填写":str);
 			}
-	        else{
+			else{
 				var str=title.resultList[0].contentDescribe1;
-				var result_id=title.resultList[0].id
+				var result_id=title.resultList[0].id;
+				var div=$(".inputs_block").closest(".h_edit_txt");
+				if(str !=undefined){
+					 div.children("dd").find("input").attr("resultId",result_id);
+				}
 				if(str !=undefined && str.indexOf("<sitg>")>-1){
 					var str=str.split("<sitg>");
 					var inputsValueList=[];
@@ -479,13 +483,13 @@ function buildResults(sec,title,readonly)
 							inputsValueList.push(inputsValue);
 						}
 					}
-				   var div=$(".inputs_block").closest(".h_edit_txt");
 				   for(var j=0;j<div.children("dd").length;j++){
-					   div.children("dd").eq(j).find("input").val(inputsValueList[j].trim()).attr("resultId",result_id);
+					   div.children("dd").eq(j).find("input").val(inputsValueList[j].trim());
 					   
 				   }
 				}
 			}
+
 		}
 		else if(title.type == 8)
 		{
