d90b72417e509e8ab768400db8b7f3e51eba3036
Wang Song <songwang@galaxyintrnet.com>
2017-12-04 11:38:23 +0800

    竞争 - 竞争对手信息变更时，清理无效数据

--- galaxy-sop-api/src/main/java/com/galaxyinternet/dao/hologram/InformationResultDao.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/dao/hologram/InformationResultDao.java
@@ -6,4 +6,8 @@ import com.galaxyinternet.model.hologram.InformationResult;
 
 public interface InformationResultDao extends BaseDao<InformationResult, Long>{
 	List<InformationResult> selectResultByRelate(InformationResult ir);
+	/**
+	 * 竞争对手信息变更时，清理无效数据
+	 */
+	int deleteNonexistent();
 }
