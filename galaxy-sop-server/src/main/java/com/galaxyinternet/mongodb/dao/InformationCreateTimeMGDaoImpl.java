73ceeb9909c357e5d8c07981a16371562f80e0b1
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-12-05 10:51:40 +0800

    update

--- galaxy-sop-server/src/main/java/com/galaxyinternet/mongodb/dao/InformationCreateTimeMGDaoImpl.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/mongodb/dao/InformationCreateTimeMGDaoImpl.java
@@ -4,9 +4,9 @@ package com.galaxyinternet.mongodb.dao;
 import org.springframework.stereotype.Service;
 
 import com.galaxyinternet.framework.core.dao.impl.MongodbBaseDaoImpl;
-import com.galaxyinternet.mongodb.model.InformationModelMG;
+import com.galaxyinternet.mongodb.model.InformationCreateTimeMG;
 
 @Service("com.galaxyinternet.mongodb.dao.InformationCreateTimeMGDao")
-public class InformationCreateTimeMGDaoImpl extends MongodbBaseDaoImpl<InformationModelMG, String> implements InformationCreateTimeMGDao {
+public class InformationCreateTimeMGDaoImpl extends MongodbBaseDaoImpl<InformationCreateTimeMG, String> implements InformationCreateTimeMGDao {
 	
 }
