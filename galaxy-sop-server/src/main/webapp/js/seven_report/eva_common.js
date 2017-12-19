2631afa161ffe793e71eccddb32c1533ce8d0058
liulijun <11141736405@qq.com>
2017-12-19 18:57:56 +0800

    weidafen

--- galaxy-sop-server/src/main/webapp/js/seven_report/eva_common.js
+++ galaxy-sop-server/src/main/webapp/js/seven_report/eva_common.js
@@ -200,6 +200,40 @@ function showResultAndScoreList(relateId)
 					$("#table_box").validate();
 					//渲染数据结束对字体颜色进行操作
 					font_color($(".condition"));
+					//权限判断， 没有编辑权限
+                    if(!isEditable || isEditable != 'true'){
+                        $("#save-rpt-btn").remove();
+                        $.each($(".score-column"),function(){
+                            var _input =$(this).find("input").val();
+                            var _select =$(this).find("select").val();  
+                            if(_input==undefined&&_select==undefined){return;}
+                            if($(this).hasClass("heightL")){                                 
+                            	var _i=_input==""?"-":_input;
+                            	var _s=_select=="请选择"?"未打分":_select; 
+                            	var res=_s +"/"+ _i+"%";
+                    			$(this).html(res);  
+                            	$(this).css("color","#b2b2b2");
+                            	if(res!="未打分/-%"){
+                            		$(this).css("color","#333");
+                            	}
+                            	return;
+                            }   
+                            if(_input==""||_input==undefined){
+                                $(this).html("未打分");
+                                $(this).css("color","#b2b2b2")
+                            }else{
+                                $(this).html(_input); 
+                                $(this).css("color","#333")
+                            }
+                            if(_select=="请选择"||_select==undefined){
+                                $(this).html("未打分");
+                                $(this).css("color","#b2b2b2")
+                            }else{
+                                $(this).html(_select);  
+                                $(this).css("color","#333")
+                            }
+                        })
+					}
 				}
 			}
 		);
