0340b54445d52f433f1804e579443bb8490b3e5b
chujinglei <chujinglei@galaxyinternet.com>
2017-12-22 08:42:15 +0800

    4类型

--- galaxy-sop-server/src/main/webapp/js/hologram/auto_show.js
+++ galaxy-sop-server/src/main/webapp/js/hologram/auto_show.js
@@ -577,10 +577,12 @@ function buildResultsDraft(sec,title,readonly)
 			$("textarea[data-title-id='"+title.id+"']").val('');
 			$("textarea[data-title-id='"+title.id+"']").closest('div.mb_24,dl.h_edit_txt').find('dt').attr("tochange",true);
 		}else if(title.type == 14){
-			$('select[data-id="' + title.id + '"],select[data-title-id="' + title.id + '"]').val('1');
+			$('select[data-title-id="' + title.id + '"]').val('1');
+			$('select[name="' + title.id + '"]').val('');   //全息报告单独处理
 			$('select[data-id="' + title.id + '"],select[data-title-id="' + title.id + '"]').closest('div.mb_24,dl.h_edit_txt').find('dt').attr("tochange",true);
 		}else if(title.type == 4){
-			$('select[data-id="' + title.id + '"],select[data-title-id="' + title.id + '"]').val('1');
+			$('select[data-title-id="' + title.id + '"]').val('1');
+			$('select[name="' + title.id + '"]').val('');   //全息报告单独处理
 			$('select[data-id="' + title.id + '"],select[data-title-id="' + title.id + '"]').closest('div.mb_24,dl.h_edit_txt').find('dt').attr("tochange",true);
 		}else if(title.type == 15){
 			var textareas = $("textarea[data-title-id='" + title.id + "'][data-type='15']");
