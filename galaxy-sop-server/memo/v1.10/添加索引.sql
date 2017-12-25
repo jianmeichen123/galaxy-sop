6a5f9b06648f1a9f7378b1ec0c7396b3970ee40e
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-12-25 10:02:24 +0800

    1111

--- galaxy-sop-server/memo/v1.10/添加索引.sql
+++ galaxy-sop-server/memo/v1.10/添加索引.sql
@@ -1,7 +1,2 @@
-INSERT INTO `fx_db`.`information_title` (`id`, `parent_id`, `code`, `name`, `index_no`, `type`, `sign`, `is_show`, `is_valid`, `placeholder`, `is_must`, `val_rule`, `val_rule_mark`) VALUES ('1119', '1117', 'NO1_2_6', '投资经理意见', '112.5', '8', '2', 't', '0', '简要表述下怎么看此项目、为什么推荐、估值逻辑等', '0', '1', '500');
-
--- 项目来源“事业部”调整成“自开发”
-UPDATE `fx_db`.`dict` SET `name`='自开发' WHERE `id`='234';
-
 CREATE TABLE sop_file_history LIKE sop_file;
 ALTER TABLE sop_file_history ADD COLUMN file_id BIGINT(20) NOT NULL;
