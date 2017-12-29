f1073d658321248d8a841714c7ecf7ac375f65ea
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-12-29 14:41:54 +0800

    update

--- galaxy-sop-server/memo/v1.12/alter.sql
+++ galaxy-sop-server/memo/v1.12/alter.sql
@@ -1,2 +1,3 @@
+--项目表添加是否删除字段
 ALTER TABLE `fx_db`.`sop_project` 
-ADD COLUMN `id_delete` INT(11) NULL DEFAULT 0 AFTER `business_type_code`;
+ADD COLUMN `id_delete` INT(11) NULL DEFAULT 1 AFTER `business_type_code`;
