966bbf02172377e232a5dd980176555195cb7967
jianmeichen123 <jianmeichen@galaxyinternet.com>
2017-12-29 15:17:27 +0800

    uodate

--- galaxy-sop-server/memo/v1.12/alter.sql
+++
@@ -1,6 +0,0 @@
---项目表添加是否删除字段
-ALTER TABLE `fx_db`.`sop_project` 
-ADD COLUMN `id_delete` INT(11) NULL DEFAULT 1 AFTER `business_type_code`;
---项目表添加删除原因字段
-ALTER TABLE `fx_db`.`sop_project` 
-ADD COLUMN `delete_reason` VARCHAR(225) NULL AFTER `id_delete`;
