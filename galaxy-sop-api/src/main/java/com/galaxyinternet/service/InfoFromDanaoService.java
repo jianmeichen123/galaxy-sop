26e4b1551f061b66148a73e75ff146f001512a45
zhangfeng <fengzhang@falaxyinternet.com>
2018-01-08 11:26:35 +0800

    danao data

--- galaxy-sop-api/src/main/java/com/galaxyinternet/service/InfoFromDanaoService.java
+++ galaxy-sop-api/src/main/java/com/galaxyinternet/service/InfoFromDanaoService.java
@@ -1,18 +1,9 @@
 package com.galaxyinternet.service;
 
-import java.util.List;
-import java.util.Map;
-
-import com.galaxyinternet.model.DongNao.DnProject;
-import org.springframework.data.domain.Pageable;
-
-import com.galaxyinternet.bo.project.ProjectBo;
 import com.galaxyinternet.framework.core.model.Page;
-import com.galaxyinternet.framework.core.model.PageRequest;
-import com.galaxyinternet.framework.core.service.BaseService;
-import com.galaxyinternet.model.project.PersonPool;
-import com.galaxyinternet.model.project.Project;
-import com.galaxyinternet.model.sopfile.SopFile;
+import com.galaxyinternet.model.DaNao.DnProject;
+
+import java.util.Map;
 
 public interface InfoFromDanaoService {
 
