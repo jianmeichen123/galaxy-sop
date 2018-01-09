6f3bfcde01cc38a191efe029499ea89f1ec7574e
liulijun <11141736405@qq.com>
2018-01-09 16:45:25 +0800

    m

--- galaxy-sop-server/src/main/webapp/js/common.js
+++ galaxy-sop-server/src/main/webapp/js/common.js
@@ -1,5 +1,5 @@
 //获取参数
-
+  
 function getURLParameter(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
