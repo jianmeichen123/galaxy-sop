25c44efdbb2dcbad74f43df2e9eab2bfce394e70
wenduan <wenduan@galaxyinternet.com>
2018-01-27 17:34:05 +0800

    update

--- galaxy-sop-server/memo/v1.11/历史数据处理.sql
+++ galaxy-sop-server/memo/v1.11/历史数据处理.sql
@@ -1,3 +1,5 @@
 -- 操作日志对象数据变更
 UPDATE `fx_db`.`sop_operation_logs` SET `operation_content`='拨付凭证' WHERE `operation_content`='打款凭证';
 UPDATE `fx_db`.`sop_operation_logs` SET `operation_content`='工商转让' WHERE `operation_content`='工商变更';
+
+UPDATE `fx_db`.`sop_task` SET `is_delete`='1' where task_name in('完善简历');
