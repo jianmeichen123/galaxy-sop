ec2dd63484fac97d04ff1c8648f7906ce22afed7
Wang Song <songwang@galaxyintrnet.com>
2018-02-03 09:53:17 +0800

    填写标准

--- galaxy-sop-server/src/main/java/com/galaxyinternet/standard/controller/StandarcController.java
+++ galaxy-sop-server/src/main/java/com/galaxyinternet/standard/controller/StandarcController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.galaxyinternet.common.utils.WebUtils;
 import com.galaxyinternet.framework.core.model.Page;
 import com.galaxyinternet.framework.core.model.PageRequest;
 import com.galaxyinternet.framework.core.model.ResponseData;
@@ -65,6 +67,7 @@ public class StandarcController
 	}
 
 	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
 	public ResponseData<Standard> get(@PathVariable Long id)
 	{
 		ResponseData<Standard> data = new ResponseData<>();
@@ -83,6 +86,7 @@ public class StandarcController
 	}
 	
 	@RequestMapping(value = "/getByCode/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
 	public ResponseData<Standard> getByCode(@PathVariable String code)
 	{
 		ResponseData<Standard> data = new ResponseData<>();
@@ -97,6 +101,32 @@ public class StandarcController
 			if (logger.isDebugEnabled())
 			{
 				logger.error(String.format("查询失败,Code:%s", code), e);
+			}
+		}
+		return data;
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseData<Standard> save(@RequestBody Standard vo)
+	{
+		ResponseData<Standard> data = new ResponseData<>();
+		try
+		{
+			Assert.notNull(vo,"信息不完整");
+			Assert.notNull(vo.getId(), "ID为空");
+			Assert.notNull(vo.getStandardDetails(), "内容为空");
+			Standard entity = new Standard();
+			entity.setId(vo.getId());
+			entity.setStandardDetails(vo.getStandardDetails());
+			entity.setUpdatedId(WebUtils.getUserFromSession().getId());
+			entity.setUpdatedTime(System.currentTimeMillis());
+			service.updateById(entity);
+		} catch (Exception e)
+		{
+			if (logger.isDebugEnabled())
+			{
+				logger.error(String.format("保存失败,Code:%s", vo), e);
 			}
 		}
 		return data;
