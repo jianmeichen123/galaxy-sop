7b76e4f7eec71c78d9762b8edeeeed3aaacc3668
liulijun <11141736405@qq.com>
2018-02-05 18:05:10 +0800

    css

--- galaxy-sop-server/src/main/webapp/js/axure.js
+++ galaxy-sop-server/src/main/webapp/js/axure.js
@@ -101,8 +101,12 @@
 					ht = parseInt($(_this.id).outerHeight(true));
 				var win_w = $(window).width(),
 					win_h = $(window).height(),
-					win_x = Math.round(win_w/2),
-					win_y = Math.round(win_h/2);
+					block_w =$(_this.id).width();
+					block_h =$(_this.id).height();
+					win_x = "50%",
+					win_y =  "50%";
+					trans_x = -Math.round(block_w/2),
+					trans_y = -Math.round(block_h/2);
 				//背景设置高度+显示
 				$("#popbg,#popbg iframe").css({"height":win_h});
 				$("#popbg01").css({"height":win_h});
@@ -117,6 +121,10 @@
 					x:win_x,
 					y:win_y
 				});
+				$(_this.id).css({
+					"margin-left":trans_x,
+					"margin-top":trans_y,
+				})
 				$(_this.id).animate({
 					opacity:1
 				},400);
