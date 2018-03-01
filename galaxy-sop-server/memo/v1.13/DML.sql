56db1d1b59967d1f07305629fe6af7bad0b1d76e
jianmeichen123 <jianmeichen@galaxyinternet.com>
2018-03-01 17:02:16 +0800

    1111

--- galaxy-sop-server/memo/v1.13/DML.sql
+++ galaxy-sop-server/memo/v1.13/DML.sql
@@ -1,3 +1,3 @@
 INSERT INTO `resource` (`Id`, `parent_id`, `resource_code`, `resource_name`, `resource_type`, `resource_url`, `resource_order`, `resource_desc`, `product_type`, `is_del`, `is_outtage`, `create_time`, `create_id`, `update_time`, `update_id`, `company_id`, `style`)
- VALUES('1701','0','standard','系统通知','1','sop/galaxy/systemMessage/tabSystemMessage/','3900','menu展示项，由此进入填写标准页面','0','0','0',NULL,NULL,NULL,NULL,'1','224');
+ VALUES('1701','0','systemMessage','系统通知','1','sop/galaxy/systemMessage/tabSystemMessage','3900','menu展示项，由此进入填写标准页面','0','0','0',NULL,NULL,NULL,NULL,'1','224');
  
